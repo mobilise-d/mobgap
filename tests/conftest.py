7ad215109b6ecff579aacf8d27ddea7ab4f5dba1
Arne Küderle <a.kuederle@gmail.com>
2023-09-01 15:01:20 +0200

    Added dummy test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+import pytest
 from tpcp.testing import PyTestSnapshotTest
 
 
---
+++ tests/test_import.py
@@ -0,0 +1,8 @@
+"""This is a dummy test to ensure that the test-setup works as expected."""
+
+
+def test_import():
+    """Test that the package can be imported."""
+    import gaitlink
+
+    assert gaitlink is not None
