a7c2208a1b4d92367e2da86bd65cd037d610fe07
Arne Küderle <a.kuederle@gmail.com>
2023-09-01 14:52:47 +0200

    Added tpcp, gaitmap, configered snapshot tests

---
+++ tests/conftest.py
@@ -0,0 +1,14 @@
+from tpcp.testing import PyTestSnapshotTest
+
+
+@pytest.fixture()
+def snapshot(request):
+    with PyTestSnapshotTest(request) as snapshot_test:
+        yield snapshot_test
+
+
+def pytest_addoption(parser):
+    group = parser.getgroup("snapshottest")
+    group.addoption(
+        "--snapshot-update", action="store_true", default=False, dest="snapshot_update", help="Update the snapshots."
+    )
