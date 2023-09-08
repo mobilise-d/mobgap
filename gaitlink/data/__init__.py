9567d8888fb9f19405ddf75ed46a78790d988fc6
Arne Küderle <a.kuederle@gmail.com>
2023-09-08 12:43:14 +0200

    linting

--- gaitlink/data/__init__.py
+++ gaitlink/data/__init__.py
@@ -1,5 +1,5 @@
 """Helper to load data and example in the various supported formats."""
 
-from gaitlink.data._mobilised_matlab_loader import load_mobilised_matlab_format, MobilisedTestData, MobilisedMetadata
+from gaitlink.data._mobilised_matlab_loader import MobilisedMetadata, MobilisedTestData, load_mobilised_matlab_format
 
 __all__ = ["load_mobilised_matlab_format", "MobilisedTestData", "MobilisedMetadata"]
