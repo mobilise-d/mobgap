dd3e265568f90a3a95c0bc7a5580e9a9c2ee5313
Arne Küderle <a.kuederle@gmail.com>
2023-09-11 11:19:19 +0200

    Changed how example data loading works.

--- gaitlink/data/__init__.py
+++ gaitlink/data/__init__.py
@@ -1,5 +1,5 @@
 """Helper to load data and example in the various supported formats."""
-from gaitlink.data._example_data import get_lab_example_data_path
+from gaitlink.data._example_data import get_all_lab_example_data_paths
 from gaitlink.data._mobilised_matlab_loader import MobilisedMetadata, MobilisedTestData, load_mobilised_matlab_format
 
-__all__ = ["load_mobilised_matlab_format", "MobilisedTestData", "MobilisedMetadata", "get_lab_example_data_path"]
+__all__ = ["load_mobilised_matlab_format", "MobilisedTestData", "MobilisedMetadata", "get_all_lab_example_data_paths"]
--- tests/test_data/test_example_data_loader.py
+++ tests/test_data/test_example_data_loader.py
@@ -1,8 +1,16 @@
-from gaitlink.data import get_lab_example_data_path
+from gaitlink.data import get_all_lab_example_data_paths
 from tests import PROJECT_ROOT
 
 
 def test_get_lab_example_data_path():
-    path = get_lab_example_data_path("HA", "002")
+    paths = get_all_lab_example_data_paths()
 
-    assert PROJECT_ROOT / "example_data/" / "data" / "lab" / "HA" / "002" == path
+    assert len(paths) == 3
+
+    assert all(p.is_relative_to(PROJECT_ROOT / "example_data/" / "data" / "lab") for p in paths.values())
+
+    assert all((p / "data.mat").is_file() for p in paths.values())
+    assert all((p / "infoForAlgo.mat").is_file() for p in paths.values())
+
+    example_path = paths[("HA", "002")]
+    assert PROJECT_ROOT / "example_data/" / "data" / "lab" / "HA" / "002" == example_path
