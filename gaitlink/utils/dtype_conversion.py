7fc01bca3e488e97a538169ef07da4387b41ec73
Arne Küderle <a.kuederle@gmail.com>
2023-10-04 11:56:54 +0200

    Added tests for dtype conversion

--- gaitlink/utils/dtype_conversion.py
+++ gaitlink/utils/dtype_conversion.py
@@ -1,4 +1,4 @@
-from typing import Union, TypeVar, Any, Tuple, Callable
+from typing import Any, Callable, TypeVar, Union
 
 import numpy as np
 import pandas as pd
@@ -27,8 +27,20 @@ def is_dflike(data: Any) -> bool:
     return isinstance(data, (pd.Series, pd.DataFrame, np.ndarray))
 
 
-def dflike_as_2d_array(data: DfLikeT) -> Tuple[np.ndarray, Callable[[np.ndarray], DfLikeT]]:
+def dflike_as_2d_array(data: DfLikeT) -> tuple[np.ndarray, Callable[[np.ndarray], DfLikeT]]:
     """Convert the passed data to a 2d numpy array and return a function to convert it back to the original datatype.
+
+    This supports the following conversions:
+
+    - ``pd.Series`` with length ``n``-> ``np.ndarray`` with shape ``(1, n)``
+    - ``pd.DataFrame`` with shape ``(m, n)`` -> ``np.ndarray`` with shape ``(m, n)``
+    - ``np.ndarray`` with shape ``(n)`` -> ``np.ndarray`` with shape ``(1, n)``
+    - ``np.ndarray`` with shape ``(m, n)`` -> ``np.ndarray`` with shape ``(m, n)``
+
+    Arrays with 0 or more than 2 dimensions are not supported.
+    Conversion of ``pd.DataFrame`` and ``pd.Series`` objects will attempt to not copy the data and will preserve the
+    index.
+
 
     Parameters
     ----------
@@ -50,13 +62,15 @@ def dflike_as_2d_array(data: DfLikeT) -> Tuple[np.ndarray, Callable[[np.ndarray]
         raise TypeError("The passed data is not dataframe-like (i.e. a dataframe, series, or numpy array).")
 
     if isinstance(data, np.ndarray):
+        if data.ndim > 2 or data.ndim == 0:
+            raise ValueError("The passed data must have 1 or 2 dimensions.")
         if data.ndim == 1:
-            data = data.reshape(-1, 1)
-        elif data.ndim > 2:
-            raise ValueError("The passed data has more than two dimensions.")
+            return data.reshape(1, -1), lambda x: x.reshape(-1)
+        return data, lambda x: x
 
-        return data, lambda x: x.reshape(data.shape)
     if isinstance(data, pd.Series):
-        return data.to_numpy().reshape(-1, 1), lambda x: pd.Series(x.reshape(data.shape), index=data.index, copy=False)
+        return data.to_numpy().reshape(1, -1), lambda x: pd.Series(
+            x.reshape(data.shape), index=data.index, copy=False, name=data.name
+        )
 
     return data.to_numpy(), lambda x: pd.DataFrame(x, columns=data.columns, index=data.index, copy=False)
