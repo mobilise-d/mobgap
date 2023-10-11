b32a8dfb105b7198c94ad80de3ff4aa8414066cd
Arne Küderle <a.kuederle@gmail.com>
2023-10-11 12:48:10 +0200

    comments and todos

--- gaitlink/wba/__init__.py
+++ gaitlink/wba/__init__.py
@@ -1,3 +1,4 @@
+"""Utilities for grouping detected stride candidates in compliant walking bouts."""
 from gaitlink.wba._interval_criteria import BaseIntervalCriteria, IntervalDurationCriteria, IntervalParameterCriteria
 from gaitlink.wba._stride_selection import StrideSelection, default_mobilised_stride_selection_rules
 
