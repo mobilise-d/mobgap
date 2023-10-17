238d5320fb7b4b2bf94eea5a88f91b95a306a1f1
Arne Küderle <a.kuederle@gmail.com>
2023-10-17 11:17:53 +0200

    Typo fixed

--- examples/data_transform/_01_generic_data_transforms.py
+++ examples/data_transform/_01_generic_data_transforms.py
@@ -4,7 +4,7 @@
 Generic Data Transforms
 =======================
 
-Besides explicit gait specif algorithms, we also provide a set of generic data transforms, which can be used as part
+Besides explicit gait specific algorithms, we also provide a set of generic data transforms, which can be used as part
 of an algorithm or to pre-process the data before applying a gait specific algorithm.
 
 Examples for such generic data transforms are:
