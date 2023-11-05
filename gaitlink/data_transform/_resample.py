99821ee6dd159d038e8b2e0aedc420ee1ed90466
Zamal Ali <87001156+zamalali@users.noreply.github.com>
2023-11-05 20:00:24 +0100

    Test cases modified

--- gaitlink/data_transform/_resample.py
+++ gaitlink/data_transform/_resample.py
@@ -5,27 +5,53 @@ from gaitlink.data_transform.base import BaseTransformer
 
 
 class Resample(BaseTransformer):
-    def __init__(self, target_sampling_rate_hz):
+    """
+    Parameters
+    ----------
+    target_sampling_rate_hz : float, optional
+        The target sampling rate in Hertz. Default is 100.0.
+
+    Attributes
+    ----------
+    target_sampling_rate_hz : float
+        The target sampling rate in Hertz.
+    """
+    def __init__(self, target_sampling_rate_hz=100.0):
         self.target_sampling_rate_hz = target_sampling_rate_hz
-        self.transformed_data_ = None
+        # self.transformed_data_ = None  # Initialize transformed_data_ to None
+    def transform(self, data: pd.DataFrame = None, *, sampling_rate_hz: float = None):
+        """
+        Resample the input data.
 
-    def transform(self, data: pd.DataFrame, *, sampling_rate_hz: float):
-        # Create a copy of the input data for consistency
-        self.transformed_data_ = data.copy()
+        Parameters
+        ----------
+        data : pd.DataFrame, optional
+            The input data to be resampled.
 
-        if sampling_rate_hz == self.target_sampling_rate_hz:
-            # No need to resample if the sampling rates match
-            return self
+        sampling_rate_hz : Target Sampling Rate, optional
 
-        # Calculate the resampling factor as a float
-        resampling_factor = self.target_sampling_rate_hz / sampling_rate_hz
+        Returns
+        -------
+        resampled_data : pd.DataFrame
+            The resampled data as a Pandas DataFrame.
+        """
+        if data is not None and sampling_rate_hz is not None:
+            # Create a copy of the input data for consistency
+            self.transformed_data_ = data.copy()
 
-        resampled_data = signal.resample(data, int((len(data) * resampling_factor)))
+            if sampling_rate_hz == self.target_sampling_rate_hz:
+                # No need to resample if the sampling rates match
+                return self
 
-        # Create a DataFrame from the resampled data
-        resampled_df = pd.DataFrame(data=resampled_data, columns=data.columns)
+            # Calculate the resampling factor as a float
+            resampling_factor = self.target_sampling_rate_hz / sampling_rate_hz
 
-        # Update the 'transformed_data_' attribute with the resampled DataFrame
-        self.transformed_data_ = resampled_df
+            resampled_data = signal.resample(data, int((len(data) * resampling_factor)))
+
+            # Create a DataFrame from the resampled data
+            resampled_df = pd.DataFrame(data=resampled_data, columns=data.columns)
+
+            # Update the 'transformed_data_' attribute with the resampled DataFrame
+            self.transformed_data_ = resampled_df
 
         return self
--- tests/test_data_transform/test_resample.py
+++ tests/test_data_transform/test_resample.py
@@ -2,18 +2,31 @@ import pandas as pd
 import pytest
 from gaitlink.data_transform._resample import Resample
 from scipy.signal import resample
+from tpcp.testing import TestAlgorithmMixin
+from gaitlink.data import LabExampleDataset
 
+class TestMetaResample(TestAlgorithmMixin):
+    ALGORITHM_CLASS = Resample
+    __test__ = True
 
-# Meta Tests
-class TestMetaResample:
-    @pytest.mark.parametrize("target_sampling_rate_hz", [100.0, 200.0])
-    def test_target_sampling_rate(self, target_sampling_rate_hz):
-        resampler = Resample(target_sampling_rate_hz)
-        assert resampler.target_sampling_rate_hz == target_sampling_rate_hz
+    @pytest.fixture
+    def after_action_instance(self):
+        # Creating an instance of your Resample class with some initial conditions
+        resampler = self.ALGORITHM_CLASS(target_sampling_rate_hz=100.0)
+
+        # Use some random input data for the meta-test (actual values might not be important)
+        input_data = pd.DataFrame({"column1": [1, 2, 3], "column2": [4, 5, 6]})
+        current_sampling_rate = 50.0  # For example purposes
+
+        # Perform the resampling action using your Resample class
+        result = resampler.transform(input_data, sampling_rate_hz=current_sampling_rate)
+
+        # Return the Resample class instance with initial conditions
+        return resampler
 
 # Unit Tests
 class TestResample:
-    @pytest.mark.parametrize("source_sampling_rate, target_sampling_rate", [(100.0, 100.0), (50.0, 100.0)])
+    @pytest.mark.parametrize("source_sampling_rate, target_sampling_rate", [(100.0, 100.0), (50.0, 100.0), (100.0, 50.0)])
     def test_resample_transform(self, source_sampling_rate, target_sampling_rate):
         # Create a Resample instance with the target sampling rate
         resampler = Resample(target_sampling_rate_hz=target_sampling_rate)
@@ -23,40 +36,37 @@ class TestResample:
 
         # Perform the transformation with the source sampling rate
         transformed_data = resampler.transform(sample_data, sampling_rate_hz=source_sampling_rate)
-        resampling_factor = target_sampling_rate / source_sampling_rate
 
         # Calculate the expected output using scipy.signal.resample
+        resampling_factor = target_sampling_rate / source_sampling_rate
         expected_output = pd.DataFrame(resample(sample_data, int(len(sample_data) * resampling_factor)))
         expected_output.columns = ['acc_x', 'acc_y']
 
-        pd.testing.assert_frame_equal(transformed_data.transformed_data_, expected_output)
-
-    @pytest.mark.parametrize("source_sampling_rate, target_sampling_rate", [(100.0, 100.0), (50.0, 100.0)])
-    def test_resample_attribute_update(self, source_sampling_rate, target_sampling_rate):
-        # Create a Resample instance with the target sampling rate
-        resampler = Resample(target_sampling_rate_hz=target_sampling_rate)  # Provide the target_sampling_rate
-
-        # Create a sample DataFrame
-        sample_data = pd.DataFrame({'acc_x': [1.0, 2.0, 3.0, 4.0], 'acc_y': [0.5, 1.0, 1.5, 2.0]})
-
-        # Perform the transformation with the source sampling rate
-        transformed_data = resampler.transform(sample_data, sampling_rate_hz=source_sampling_rate)
-
         # Check if the 'transformed_data_' attribute is updated with the transformed data
         expected_resampled_data = pd.DataFrame(
-            data=resample(sample_data, int(len(sample_data) * (target_sampling_rate / source_sampling_rate))),
+            data=resample(sample_data, int(len(sample_data) * resampling_factor)),
             columns=sample_data.columns
         )
 
         # Compare the transformed data with the manually resampled data
-        pd.testing.assert_frame_equal(transformed_data.transformed_data_, expected_resampled_data)
+        pd.testing.assert_frame_equal(transformed_data.transformed_data_, expected_output)
 
+        # Check if the transformed data is a copy when source and target sampling rates are identical
+        if source_sampling_rate == target_sampling_rate:
+            assert transformed_data is not sample_data  # Check that transformed_data is not the same object as sample_data
+
+
+
+example_data = LabExampleDataset()
+ha_example_data = example_data.get_subset(cohort="HA")
+single_test = ha_example_data.get_subset(participant_id="002", test="Test11", trial="Trial1")
+df = single_test.data["LowerBack"]
 # Regression Test
 class TestResampleRegression:
     @pytest.mark.parametrize("target_sampling_rate", [100.0, 200.0])
     def test_regression_test(self, target_sampling_rate):
         # Load your real data or create a synthetic dataset
-        real_data = pd.DataFrame({'acc_x': [1.0, 2.0, 3.0, 4.0], 'acc_y': [0.5, 1.0, 1.5, 2.0]})
+        real_data = df
 
         # Create a Resample instance with the target sampling rate
         resampler = Resample(target_sampling_rate_hz=target_sampling_rate)
@@ -66,7 +76,7 @@ class TestResampleRegression:
 
         # Calculate the expected output using scipy.signal.resample
         expected_output = pd.DataFrame(resample(real_data, int(len(real_data) * (target_sampling_rate / 50.0)), axis=0))
-        expected_output.columns = ['acc_x', 'acc_y']
+        expected_output.columns = ['acc_x', 'acc_y', 'acc_z', 'gyr_x', 'gyr_y', 'gyr_z']
         # Check if the transformed data matches the expected output
         pd.testing.assert_frame_equal(transformed_data.transformed_data_, expected_output)
 
