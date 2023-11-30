bce21ae00905c1fdd0e168e76a0eba59c42b9090
Arne Küderle <a.kuederle@gmail.com>
2023-11-30 16:02:13 +0100

    First tests and some bugfixes

--- tests/test_cad/test_cad_from_ic.py
+++ tests/test_cad/test_cad_from_ic.py
@@ -1,9 +1,11 @@
 import numpy as np
 import pandas as pd
 import pytest
+from pandas._testing import assert_series_equal
 from tpcp.testing import TestAlgorithmMixin
 
 from gaitlink.cad import CadFromIc
+from gaitlink.cad._cad_from_ic import CadFromIcDetector
 
 
 class TestMetaCadFromIc(TestAlgorithmMixin):
@@ -18,3 +20,108 @@ class TestMetaCadFromIc(TestAlgorithmMixin):
             initial_contacts=pd.Series(np.arange(0, 100, 5)),
             sampling_rate_hz=40.0,
         )
+
+
+class TestMetaCadFromIcDetector(TestAlgorithmMixin):
+    __test__ = True
+
+    ALGORITHM_CLASS = CadFromIcDetector
+
+    @pytest.fixture()
+    def after_action_instance(self):
+        return self.ALGORITHM_CLASS().calculate(
+            pd.DataFrame(np.zeros((100, 3)), columns=["acc_x", "acc_y", "acc_z"]),
+            initial_contacts=pd.Series(np.arange(0, 100, 5)),
+            sampling_rate_hz=40.0,
+        )
+
+
+class TestCadFromIc:
+    @pytest.mark.parametrize("sampling_rate_hz", [10.0, 20.0, 40.0])
+    @pytest.mark.parametrize("fixed_step_size", [5, 10, 20])
+    def test_naive(self, sampling_rate_hz, fixed_step_size):
+        n_samples = 100
+
+        data = pd.DataFrame(np.zeros((n_samples, 3)), columns=["acc_x", "acc_y", "acc_z"])
+        initial_contacts = pd.Series(np.arange(0, n_samples + 1, fixed_step_size))
+        data = data.iloc[: initial_contacts.iloc[-1]]
+
+        cad = CadFromIc().calculate(data, initial_contacts, sampling_rate_hz=sampling_rate_hz)
+        cadence = cad.cadence_per_sec_
+
+        assert len(cadence) == len(data) // sampling_rate_hz
+        assert_series_equal(cadence, pd.Series(np.ones(len(cadence)) * 1 / (fixed_step_size / sampling_rate_hz) * 60))
+
+    def test_large_gap_no_interpolation(self):
+        sampling_rate_hz = 40.0
+        fixed_step_size = 5
+        n_samples = 300
+
+        data = pd.DataFrame(np.zeros((n_samples, 3)), columns=["acc_x", "acc_y", "acc_z"])
+        initial_contacts = pd.Series(np.arange(0, n_samples, fixed_step_size))
+        max_gap_s = 1
+
+        n_ics_to_drop = max_gap_s * 3 * sampling_rate_hz // fixed_step_size
+
+        # We introduce a gap in the data that is not covered by the initial contacts
+        initial_contacts = initial_contacts.drop(initial_contacts.index[2 : 2 + int(n_ics_to_drop)])
+
+        data = data.iloc[: initial_contacts.iloc[-1]]
+
+        cad = CadFromIc(max_interpolation_gap_s=max_gap_s).calculate(
+            data, initial_contacts, sampling_rate_hz=sampling_rate_hz
+        )
+        cadence = cad.cadence_per_sec_
+
+        assert len(cadence) == len(data) // sampling_rate_hz
+        expected_output = np.ones(len(cadence)) * 1 / (fixed_step_size / sampling_rate_hz) * 60
+        expected_output[1:3] = np.nan
+
+        assert_series_equal(cadence, pd.Series(expected_output))
+
+    def test_small_gap_interpolation(self):
+        sampling_rate_hz = 40.0
+        fixed_step_size = 5
+        n_samples = 300
+
+        data = pd.DataFrame(np.zeros((n_samples, 3)), columns=["acc_x", "acc_y", "acc_z"])
+        initial_contacts = pd.Series(np.arange(0, n_samples, fixed_step_size))
+        max_gap_s = 6
+
+        # We drop less than max_gap_s -> should get interpolated
+        n_secs_to_drop = max_gap_s * 0.5
+        assert n_secs_to_drop > 2
+        n_ics_to_drop = n_secs_to_drop * sampling_rate_hz // fixed_step_size
+
+        # We introduce a gap in the data that is not covered by the initial contacts
+        initial_contacts = initial_contacts.drop(initial_contacts.index[2 : 2 + int(n_ics_to_drop)])
+
+        data = data.iloc[: initial_contacts.iloc[-1]]
+
+        cad = CadFromIc(max_interpolation_gap_s=max_gap_s).calculate(
+            data, initial_contacts, sampling_rate_hz=sampling_rate_hz
+        )
+        cadence = cad.cadence_per_sec_
+
+        assert len(cadence) == len(data) // sampling_rate_hz
+        expected_output = np.ones(len(cadence)) * 1 / (fixed_step_size / sampling_rate_hz) * 60
+        assert_series_equal(cadence, pd.Series(expected_output))
+
+    def test_no_extrapolation(self):
+        sampling_rate_hz = 40.0
+        fixed_step_size = 5
+        n_samples = 300
+
+        data = pd.DataFrame(np.zeros((n_samples, 3)), columns=["acc_x", "acc_y", "acc_z"])
+        # We remove ICs at the beginning and end
+        initial_contacts = pd.Series(np.arange(2 * sampling_rate_hz, n_samples - 2 * sampling_rate_hz, fixed_step_size))
+
+        cad = CadFromIc().calculate(data, initial_contacts, sampling_rate_hz=sampling_rate_hz)
+        cadence = cad.cadence_per_sec_
+
+        assert len(cadence) == len(data) // sampling_rate_hz
+        expected_output = np.ones(len(cadence)) * 1 / (fixed_step_size / sampling_rate_hz) * 60
+        expected_output[0] = np.nan
+        expected_output[-1] = np.nan
+
+        assert_series_equal(cadence, pd.Series(expected_output))
