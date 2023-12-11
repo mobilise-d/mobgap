4abe3274a7c3c3f905fa67e3bc935907e1fc3402
Arne Küderle <a.kuederle@gmail.com>
2023-12-11 09:44:09 +0100

    Changed import structure and docstrings

--- gaitlink/pipeline/__init__.py
+++ gaitlink/pipeline/__init__.py
@@ -1,4 +1,4 @@
 """Pre-build pipelines and pipeline helpers."""
-from gaitlink.pipeline._gs_iterator import GsIterator, iter_gs
+from gaitlink.pipeline._gs_iterator import GsIterator, iter_gs, FullPipelinePerGsResult, create_aggregate_df
 
-__all__ = ["iter_gs", "GsIterator"]
+__all__ = ["iter_gs", "GsIterator", "FullPipelinePerGsResult", "create_aggregate_df"]
