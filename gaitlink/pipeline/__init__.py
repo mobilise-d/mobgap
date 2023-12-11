22a1928770a7da2e0a6e2f5d9cc8f94eeafd56ca
Arne Küderle <a.kuederle@gmail.com>
2023-12-11 10:24:53 +0100

    Better wording in examples and fixed some small issues

--- gaitlink/pipeline/__init__.py
+++ gaitlink/pipeline/__init__.py
@@ -1,4 +1,4 @@
 """Pre-build pipelines and pipeline helpers."""
-from gaitlink.pipeline._gs_iterator import GsIterator, iter_gs, FullPipelinePerGsResult, create_aggregate_df
+from gaitlink.pipeline._gs_iterator import FullPipelinePerGsResult, GsIterator, create_aggregate_df, iter_gs
 
 __all__ = ["iter_gs", "GsIterator", "FullPipelinePerGsResult", "create_aggregate_df"]
