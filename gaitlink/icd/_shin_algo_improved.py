135e04c9d321d9bba21472027eecd40b222cebfe
Arne Küderle <a.kuederle@gmail.com>
2024-01-15 14:29:59 +0100

    Some minor improvements

--- gaitlink/icd/_shin_algo_improved.py
+++ gaitlink/icd/_shin_algo_improved.py
@@ -15,42 +15,38 @@ from gaitlink.icd.base import BaseIcdDetector
 class IcdShinImproved(BaseIcdDetector):
     axis: Literal["x", "y", "z", "norm"]
 
-    def __init__(self, axis: Literal["x", "y", "z", "norm"] = "norm"):
+    _INTERNAL_FILTER_SAMPLING_RATE_HZ: int = 40
+
+    def __init__(self, axis: Literal["x", "y", "z", "norm"] = "norm") -> None:
         self.axis = axis
 
     def detect(self, data: pd.DataFrame, *, sampling_rate_hz: float, **_: Unpack[dict[str, Any]]) -> Self:
         self.data = data
         self.sampling_rate_hz = sampling_rate_hz
 
-        if self.axis == "norm":
-            # TODO: Double check correct axis
-            accN = norm(data.to_numpy(), axis=1)
-        else:
-            accN = data[self.axis].to_numpy()
+        signal = norm(data.to_numpy(), axis=1) if self.axis == "norm" else data[self.axis].to_numpy()
 
         # Resample to 40Hz to process with filters
-        current_sampling_rate = sampling_rate_hz
-        target_sampling_rate = 40
-        resampler = Resample(target_sampling_rate)
-        resampler.transform(data=accN, sampling_rate_hz=current_sampling_rate)
-        accN40 = resampler.transformed_data_
+        resampler = Resample(self._INTERNAL_FILTER_SAMPLING_RATE_HZ)
+        resampler.transform(data=signal, sampling_rate_hz=sampling_rate_hz)
+        signal_downsampled = resampler.transformed_data_
 
         # We need to intitialize the filter once to get the number of coefficients to calculate the padding.
         # This is not ideal, but works for now.
         # TODO: We should evaluate, if we need the padding at all, or if the filter methods that we use handle that
         #  correctly anyway. -> filtfilt uses padding automatically and savgol allows to actiavte padding, put uses the
-        #  default mode (polyinomal interpolation) might be suffiecent anyway, cwt might have some edeeffects, but usually
-        #  nothing to worry about.
+        #  default mode (polyinomal interpolation) might be suffiecent anyway, cwt might have some edeeffects, but
+        #  usually nothing to worry about.
         n_coefficients = len(EpflGaitFilter().coefficients[0])
 
         # Padding to cope with short data
         len_pad = 4 * n_coefficients
-        accN40_zp = np.pad(accN40, (len_pad, len_pad), "wrap")
+        signal_downsampled_padded = np.pad(signal_downsampled, (len_pad, len_pad), "wrap")
 
         # Filters
         # 1
         # TODO (future): Replace svagol and cwt with class implementation to easily expose parameters.
-        accN_filt1 = savgol_filter(accN40_zp.squeeze(), window_length=21, polyorder=7)
+        accN_filt1 = savgol_filter(signal_downsampled_padded.squeeze(), window_length=21, polyorder=7)
         # 2
         filter = EpflDedriftedGaitFilter()
         # TODO: Why is the output not used?
@@ -73,25 +69,19 @@ class IcdShinImproved(BaseIcdDetector):
         windowWidth = 15
         sigma = windowWidth / 5
         accN_filt8 = gaussian_filter(accN_filt7.squeeze(), sigma)
-        # MultiFilt
         accN_MultiFilt_rmp = accN_filt8[len_pad:-len_pad]
 
-        # TODO (for future): Upsampling here does not seem that "smart". Maybe better to detect zero crossing in low freq
-        #                    and then upsample results. Should be much more efficient.
+        # TODO (for future): Upsampling here does not seem that "smart". Maybe better to detect zero crossing in low
+        #                    freq and then upsample results. Should be much more efficient.
         # Resample to 100Hz for consistency with the original data (for ICD)
-        current_sampling_rate = 40
         resampler = Resample(sampling_rate_hz)
-        resampler.transform(data=accN_MultiFilt_rmp, sampling_rate_hz=current_sampling_rate)
+        resampler.transform(data=accN_MultiFilt_rmp, sampling_rate_hz=self._INTERNAL_FILTER_SAMPLING_RATE_HZ)
         accN_MultiFilt_rmp100 = resampler.transformed_data_
 
         # Initial contacts timings (heel strike events) detected as positive slopes zero-crossing in sample 120
         IC_lowSNR = find_zero_crossings(accN_MultiFilt_rmp100, "negative_to_positive")
 
-        self.ic_list_ = pd.DataFrame(
-            {
-                "ics": IC_lowSNR
-            }
-        )
+        self.ic_list_ = pd.DataFrame({"ics": IC_lowSNR})
 
         return self
 
@@ -124,7 +114,7 @@ def find_zero_crossings(
     Examples
     --------
     >>> signal = np.array([1, -1, 1, -1, 1])
-    >>> find_zero_crossings(signal, mode='both')
+    >>> find_zero_crossings(signal, mode="both")
     array([0, 1, 2, 3])
     """
     # Compute differences between consecutive elements
