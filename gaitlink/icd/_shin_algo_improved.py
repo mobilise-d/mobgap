4b925bbd05f9b95478ac2e1fa955db1826a663fe
Arne Küderle <a.kuederle@gmail.com>
2024-01-15 13:58:41 +0100

    Some cleanup of method

--- gaitlink/icd/_shin_algo_improved.py
+++ gaitlink/icd/_shin_algo_improved.py
@@ -1,14 +1,14 @@
-import csv
-from typing import Literal, Any
-from typing_extensions import Unpack, Self
+from typing import Any, Literal
 
 import numpy as np
 import pandas as pd
 from gaitmap.data_transform import Resample
+from numpy.linalg import norm
 from scipy.ndimage import gaussian_filter
 from scipy.signal import cwt, ricker, savgol_filter
+from typing_extensions import Self, Unpack
 
-from gaitlink.data_transform import EpflDedriftedGaitFilter
+from gaitlink.data_transform import EpflDedriftedGaitFilter, EpflGaitFilter
 from gaitlink.icd._utils import zerocros
 from gaitlink.icd.base import BaseIcdDetector
 
@@ -23,28 +23,25 @@ class IcdShinImproved(BaseIcdDetector):
         self.data = data
         self.sampling_rate_hz = sampling_rate_hz
 
-        self.ic_list_ = pd.DataFrame({"ics": _shin_algo_improved(
-            self.data[["acc_x", "acc_y", "acc_z"]], sampling_rate_hz=sampling_rate_hz, axis=self.axis
-        )})
+        self.ic_list_ = pd.DataFrame(
+            {
+                "ics": _shin_algo_improved(
+                    self.data[["acc_x", "acc_y", "acc_z"]], sampling_rate_hz=sampling_rate_hz, axis=self.axis
+                )
+            }
+        )
 
         return self
 
 
-def _shin_algo_improved(imu_acc, sampling_rate_hz, axis="norm"):
+def _shin_algo_improved(imu_acc, sampling_rate_hz, axis: Literal["x", "y", "z", "norm"] = "norm"):
     # Check if 'data' is provided, if not, set it to the default 'norm' (all axes of accelerometry)
-    if not axis or len(axis) == 0:
-        axis = "norm"
 
-    acc = imu_acc
-
-    if "x" in axis:
-        accN = acc.flatten()
-
-    elif "norm" in axis:
-        accN = np.sqrt(acc[:, 0] ** 2 + acc[:, 1] ** 2 + acc[:, 2] ** 2)
-
-    IC = []
-    IC_lowSNR = []
+    if axis == "norm":
+        # TODO: Double check correct axis
+        accN = norm(imu_acc.to_numpy(), axis=1)
+    else:
+        accN = imu_acc[axis].to_numpy()
 
     # Resample to 40Hz to process with filters
     current_sampling_rate = sampling_rate_hz
@@ -53,21 +50,15 @@ def _shin_algo_improved(imu_acc, sampling_rate_hz, axis="norm"):
     resampler.transform(data=accN, sampling_rate_hz=current_sampling_rate)
     accN40 = resampler.transformed_data_
 
-    # FIR filter TODO: update coefficients from import function
-
-    csv_file = "epfl_gait_filter.csv"
-    Num = []  # Initialize the list to store coefficients
-
-    with open(csv_file) as file:
-        reader = csv.reader(file)
-        next(reader)  # Skip the header row if there is one
-        for row in reader:
-            coefficient = float(row[0])
-            Num.append(coefficient)
+    # We need to intitialize the filter once to get the number of coefficients to calculate the padding.
+    # This is not ideal, but works for now.
+    # TODO: We should evaluate, if we need the padding at all, or if the filter methods that we use handle that
+    #  correctly anyway.
+    n_coefficients = len(EpflGaitFilter().coefficients[0])
 
     # Padding to cope with short data
-    len_padd = 4 * len(Num)
-    accN40_zp = np.pad(accN40, (len_padd, len_padd), "wrap")
+    len_pad = 4 * n_coefficients
+    accN40_zp = np.pad(accN40, (len_pad, len_pad), "wrap")
 
     # TODO: change number on files below
     # Filters
@@ -75,6 +66,7 @@ def _shin_algo_improved(imu_acc, sampling_rate_hz, axis="norm"):
     accN_filt1 = savgol_filter(accN40_zp.squeeze(), window_length=21, polyorder=7)
     # 2
     filter = EpflDedriftedGaitFilter()
+    # TODO: Why is the output not used?
     accN_filt2 = filter.filter(accN_filt1, sampling_rate_hz=40).filtered_data_
     # 3
     accN_filt3 = cwt(accN_filt1.squeeze(), ricker, [10])
@@ -95,23 +87,13 @@ def _shin_algo_improved(imu_acc, sampling_rate_hz, axis="norm"):
     sigma = windowWidth / 5
     accN_filt8 = gaussian_filter(accN_filt7.squeeze(), sigma)
     # MultiFilt
-    accN_MultiFilt_rmp = accN_filt8[len_padd:-len_padd]
+    accN_MultiFilt_rmp = accN_filt8[len_pad:-len_pad]
 
-    # Resample to 50Hz for output in seconds or to 100 for IC detection
-    # Resample to 50Hz for consistency with the original paper
-    # fs_new = 50
-    # accN_MultiFilt_rmp50 = resampInterp(accN_MultiFilt_rmp, algorithm_target_fs, fs_new);
-
-    # Initial contacts timings (heel strike events) detected as positive slopes zero-crossing in seconds
-    # IC_lowSNR = zerocros(accN_MultiFilt_rmp50, 'p')
-    # IC_lowSNR = IC_lowSNR[0]
-    # IC_lowSNR = np.round(IC_lowSNR)
-    # IC = IC_lowSNR / fs_new # in seconds
-
-    # Resample to 100Hz for consistency with the original data (for ICD) or to 50 for consistency with original paper
+    # TODO (for future): Upsampling here does not seem that "smart". Maybe better to detect zero crossing in low freq
+    #                    and then upsample results. Should be much more efficient.
+    # Resample to 100Hz for consistency with the original data (for ICD)
     current_sampling_rate = 40
-    target_sampling_rate = 100
-    resampler = Resample(target_sampling_rate)
+    resampler = Resample(sampling_rate_hz)
     resampler.transform(data=accN_MultiFilt_rmp, sampling_rate_hz=current_sampling_rate)
     accN_MultiFilt_rmp100 = resampler.transformed_data_
 
@@ -119,7 +101,5 @@ def _shin_algo_improved(imu_acc, sampling_rate_hz, axis="norm"):
     IC_lowSNR = zerocros(accN_MultiFilt_rmp100, "p")
     IC_lowSNR = IC_lowSNR[0]
     IC_lowSNR = np.round(IC_lowSNR)
-    IC = IC_lowSNR  # in sample
-    # IC = IC / target_sampling_rate  # in seconds
 
-    return IC
+    return IC_lowSNR
