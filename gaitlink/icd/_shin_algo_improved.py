df14c939ddb9f897219c3ce28e3e0e2d7f5530b9
Arne Küderle <a.kuederle@gmail.com>
2024-01-15 13:48:31 +0100

    Added basic class structure

--- gaitlink/icd/_shin_algo_improved.py
+++ gaitlink/icd/_shin_algo_improved.py
@@ -1,32 +1,53 @@
 import csv
+from typing import Literal, Any
+from typing_extensions import Unpack, Self
 
 import numpy as np
+import pandas as pd
 from gaitmap.data_transform import Resample
 from scipy.ndimage import gaussian_filter
 from scipy.signal import cwt, ricker, savgol_filter
 
 from gaitlink.data_transform import EpflDedriftedGaitFilter
 from gaitlink.icd._utils import zerocros
+from gaitlink.icd.base import BaseIcdDetector
 
 
-def shin_algo_improved(imu_acc, fs, data="norm"):
+class IcdShinImproved(BaseIcdDetector):
+    axis: Literal["x", "y", "z", "norm"]
+
+    def __init__(self, axis: Literal["x", "y", "z", "norm"] = "norm"):
+        self.axis = axis
+
+    def detect(self, data: pd.DataFrame, *, sampling_rate_hz: float, **_: Unpack[dict[str, Any]]) -> Self:
+        self.data = data
+        self.sampling_rate_hz = sampling_rate_hz
+
+        self.ic_list_ = pd.DataFrame({"ics": _shin_algo_improved(
+            self.data[["acc_x", "acc_y", "acc_z"]], sampling_rate_hz=sampling_rate_hz, axis=self.axis
+        )})
+
+        return self
+
+
+def _shin_algo_improved(imu_acc, sampling_rate_hz, axis="norm"):
     # Check if 'data' is provided, if not, set it to the default 'norm' (all axes of accelerometry)
-    if not data or len(data) == 0:
-        data = "norm"
+    if not axis or len(axis) == 0:
+        axis = "norm"
 
     acc = imu_acc
 
-    if "x" in data:
+    if "x" in axis:
         accN = acc.flatten()
 
-    elif "norm" in data:
+    elif "norm" in axis:
         accN = np.sqrt(acc[:, 0] ** 2 + acc[:, 1] ** 2 + acc[:, 2] ** 2)
 
     IC = []
     IC_lowSNR = []
 
     # Resample to 40Hz to process with filters
-    current_sampling_rate = fs
+    current_sampling_rate = sampling_rate_hz
     target_sampling_rate = 40
     resampler = Resample(target_sampling_rate)
     resampler.transform(data=accN, sampling_rate_hz=current_sampling_rate)
