2687d54895526dbe13a70bc020e112fcdca2c25f
Annika Mücke <annika-muecke@outlook.de>
2024-01-17 10:55:09 +0100

    extend gsd example snapshot tests

--- tests/test_examples/test_gsd.py
+++ tests/test_examples/test_gsd.py
@@ -1,6 +1,16 @@
 def test_gsd_iluz(snapshot):
-    from examples.gsd._01_gsd_iluz import long_trial_output, long_trial_output_modified, short_trial_output
+    from examples.gsd._01_gsd_iluz import (
+        categorized_intervals,
+        long_trial_output,
+        long_trial_output_modified,
+        matches,
+        short_trial_output,
+    )
 
     snapshot.assert_match(long_trial_output.gsd_list_, "long_trial_output")
     snapshot.assert_match(long_trial_output_modified.gsd_list_, "long_trial_output_modified")
     snapshot.assert_match(short_trial_output.gsd_list_, "short_trial_output")
+    snapshot.assert_match(categorized_intervals.tp_intervals, "tp_intervals")
+    snapshot.assert_match(categorized_intervals.fp_intervals, "fp_intervals")
+    snapshot.assert_match(categorized_intervals.fn_intervals, "fn_intervals")
+    snapshot.assert_match(matches, "matches")
