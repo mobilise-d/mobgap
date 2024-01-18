4158723b9191a32d44179a77d2fc0136e97230ca
Arne Küderle <a.kuederle@gmail.com>
2024-01-18 16:26:37 +0100

    Moved class defintion to top

--- gaitlink/gsd/_gsd_b.py
+++ gaitlink/gsd/_gsd_b.py
@@ -14,6 +14,110 @@ from gaitlink.data_transform import EpflDedriftedGaitFilter, Resample
 from gaitlink.gsd.base import BaseGsdDetector
 
 
+class GsdLowBackAcc(BaseGsdDetector):
+    def detect(self, data: pd.DataFrame, *, sampling_rate_hz: float, **_: Unpack[dict[str, Any]]) -> Self:
+        self.data = data
+        self.sampling_rate_hz = sampling_rate_hz
+
+        acc = data[["acc_x", "acc_y", "acc_z"]].to_numpy()
+
+        ALGORITHM_TARGET_SAMPLING_RATE_HZ = 40  # Sampling rate required for the algorithm
+
+        # Signal vector magnitude
+        acc_norm = np.linalg.norm(acc, axis=1)
+
+        # Resample to algorithm_target_fs
+        acc_norm_resampled = (
+            Resample(ALGORITHM_TARGET_SAMPLING_RATE_HZ)
+            .transform(acc_norm, sampling_rate_hz=sampling_rate_hz)
+            .transformed_data_
+        )
+        # NOTE: accN_resampled is slightly different in length and values to accN40 in MATLAB, plots look ok though
+
+        # Filter to enhance the acceleration signal, when low SNR, impaired, asymmetric and slow gait
+        acc_filtered = scipy.signal.savgol_filter(acc_norm_resampled, polyorder=7, window_length=21)
+        acc_filtered = EpflDedriftedGaitFilter().filter(acc_filtered, sampling_rate_hz=40).filtered_data_
+        # NOTE: Original MATLAB code calls old version of cwt (open wavelet.internal.cwt in MATLAB to inspect) in
+        #   accN_filt3=cwt(accN_filt2,10,'gaus2',1/40);
+        #   Here, 10 is the scale, gaus2 is the second derivative of a Gaussian wavelet, aka a Mexican Hat or Ricker wavelet
+        #   In Python, a scale of 7 matches the MATLAB scale of 10 from visual inspection of plots (likely due to how to two
+        #   languages initialise their wavelets), giving the line below
+        acc_filtered = scipy.signal.cwt(acc_filtered.squeeze(), scipy.signal.ricker, [7])
+        acc_filtered4 = scipy.signal.savgol_filter(acc_filtered, 11, 5)
+        acc_filtered = scipy.signal.cwt(acc_filtered4.squeeze(), scipy.signal.ricker, [7])  # See NOTE above
+        acc_filtered = scipy.ndimage.gaussian_filter(
+            acc_filtered.squeeze(), 2
+        )  # NOTE: sigma = windowWidth / 5, windowWidth = 10 (from MATLAB)
+        acc_filtered = scipy.ndimage.gaussian_filter(
+            acc_filtered.squeeze(), 2
+        )  # NOTE: sigma = windowWidth / 5, windowWidth = 10 (from MATLAB)
+        acc_filtered = scipy.ndimage.gaussian_filter(
+            acc_filtered.squeeze(), 3
+        )  # NOTE: sigma = windowWidth / 5, windowWidth = 15 (from MATLAB)
+        acc_filtered = scipy.ndimage.gaussian_filter(
+            acc_filtered.squeeze(), 2
+        )  # NOTE: sigma = windowWidth / 5, windowWidth = 10 (from MATLAB)
+
+        FALLBACK_THRESHOLD = 0.15
+
+        try:
+            active_peak_threshold = find_active_period_peak_threshold(acc_filtered, ALGORITHM_TARGET_SAMPLING_RATE_HZ)
+            signal = acc_filtered
+        except NoActivePeriodsDetectedError:
+            # If we don't find the active periods, use a fallback threshold and use
+            # a less filtered signal for further processing, for which we can better predict the threshold.
+            warnings.warn("No active periods detected, using fallback threshold")
+            active_peak_threshold = FALLBACK_THRESHOLD
+            signal = acc_filtered4
+
+        # Find extrema in signal that might represent steps
+        min_peaks, max_peaks = find_min_max_above_threshold(signal, active_peak_threshold)
+
+        # Combine steps detected by the maxima and minima
+        MIN_N_STEPS = 5
+
+        gs_from_max = find_pulse_trains(max_peaks)
+        gs_from_min = find_pulse_trains(min_peaks)
+        gs_from_max = gs_from_max[gs_from_max[:, 2] >= MIN_N_STEPS]
+        gs_from_min = gs_from_min[gs_from_min[:, 2] >= MIN_N_STEPS]
+
+        combined_final = find_intersections(
+            gs_from_max[:, :2], gs_from_min[:, :2]
+        )  # Combine the gs from the maxima and minima
+
+        if combined_final.size == 0:  # Check if no gs detected
+            return pd.DataFrame(columns=["start", "end"]).astype(int)  # Return empty df, if no gs
+
+        # Find all max_peaks withing each final gait sequence (GS)
+        steps_per_gs = [[x for x in max_peaks if gs[0] <= x <= gs[1]] for gs in combined_final]
+        n_steps_per_gs = np.array([len(steps) for steps in steps_per_gs])
+        mean_step_times = np.array([np.mean(np.diff(steps)) for steps in steps_per_gs])
+
+        # Pad each gs by 0.75*step_time before and after
+        combined_final[:, 0] = np.fix(combined_final[:, 0] - 0.75 * mean_step_times)
+        combined_final[:, 1] = np.fix(combined_final[:, 1] + 0.75 * mean_step_times)
+
+        # Filter again by number of steps, remove any gs with too few steps
+        combined_final = combined_final[n_steps_per_gs >= MIN_N_STEPS]
+
+        if combined_final.size == 0:  # Check if all gs removed
+            return pd.DataFrame(columns=["start", "end"]).astype(int)  # Return empty df if no gs
+
+        # Merge gs if time (in seconds) between consecutive gs is less than MAX_GAP_S
+        MAX_GAP_S = 3
+        combined_final = merge_intervals(combined_final, ALGORITHM_TARGET_SAMPLING_RATE_HZ * MAX_GAP_S)
+
+        # Convert back to original sampling rate
+        combined_final = combined_final * sampling_rate_hz / ALGORITHM_TARGET_SAMPLING_RATE_HZ
+
+        # Cap the start and the end of the signal using clip, incase padding extended any gs past the signal length
+        combined_final = np.clip(combined_final, 0, len(acc))
+
+        self.gsd_list_ =  pd.DataFrame(combined_final, columns=["start", "end"]).astype(int)
+
+        return self
+
+
 def hilbert_envelop(y, Smooth_window, threshold_style, DURATION):
     """NOTE: This has been edited from the original MATLAB version to remove perceived error"""
     # Calculate the analytical signal and get the envelope
@@ -153,7 +257,7 @@ def find_intersections(intervals_a: list[tuple[int, int]], intervals_b: list[tup
         An array of intervals that are the intersections of the intervals in `intervals_a` and `intervals_b`.
         Each interval is represented as a list of two integers.
 
-    """    # Create Interval Trees
+    """  # Create Interval Trees
     intervals_a_tree = IntervalTree.from_tuples(intervals_a)
     intervals_b_tree = IntervalTree.from_tuples(intervals_b)
 
@@ -169,17 +273,6 @@ def find_intersections(intervals_a: list[tuple[int, int]], intervals_b: list[tup
                 overlap_intervals.append([start, end])
 
     return merge_intervals(np.array(overlap_intervals)) if len(overlap_intervals) != 0 else np.array([])
-
-class GsdLowBackAcc(BaseGsdDetector):
-
-    def detect(self, data: pd.DataFrame, *, sampling_rate_hz: float, **_: Unpack[dict[str, Any]]) -> Self:
-        self.data = data
-        self.sampling_rate_hz = sampling_rate_hz
-
-        gsd_output = gsd_low_back_acc(data[["acc_x", "acc_y", "acc_z"]].to_numpy(), sampling_rate_hz, plot_results=True)
-        self.gsd_list_ = gsd_output
-
-        return self
 
 
 class NoActivePeriodsDetectedError(Exception):
@@ -220,106 +313,3 @@ def find_active_period_peak_threshold(signal, sampling_rate_hz) -> float:
     pksp, pksn = walkLowBack[peaks_p], -walkLowBack[peaks_n]
     pks = np.concatenate([pksp[pksp > 0], pksn[pksn > 0]])
     return np.percentile(pks, 5)  # data adaptive threshold
-
-
-def gsd_low_back_acc(acc: np.ndarray, sampling_rate_hz, plot_results=True):
-    """
-
-    :param acc:
-    :param sampling_rate_hz:
-    :param plot_results:
-    :return GSD_Output:
-    """
-    ALGORITHM_TARGET_SAMPLING_RATE_HZ = 40  # Sampling rate required for the algorithm
-
-    # Signal vector magnitude
-    acc_norm = np.linalg.norm(acc, axis=1)
-
-    # Resample to algorithm_target_fs
-    acc_norm_resampled = (
-        Resample(ALGORITHM_TARGET_SAMPLING_RATE_HZ)
-        .transform(acc_norm, sampling_rate_hz=sampling_rate_hz)
-        .transformed_data_
-    )
-    # NOTE: accN_resampled is slightly different in length and values to accN40 in MATLAB, plots look ok though
-
-    # Filter to enhance the acceleration signal, when low SNR, impaired, asymmetric and slow gait
-    acc_filtered = scipy.signal.savgol_filter(acc_norm_resampled, polyorder=7, window_length=21)
-    acc_filtered = EpflDedriftedGaitFilter().filter(acc_filtered, sampling_rate_hz=40).filtered_data_
-    # NOTE: Original MATLAB code calls old version of cwt (open wavelet.internal.cwt in MATLAB to inspect) in
-    #   accN_filt3=cwt(accN_filt2,10,'gaus2',1/40);
-    #   Here, 10 is the scale, gaus2 is the second derivative of a Gaussian wavelet, aka a Mexican Hat or Ricker wavelet
-    #   In Python, a scale of 7 matches the MATLAB scale of 10 from visual inspection of plots (likely due to how to two
-    #   languages initialise their wavelets), giving the line below
-    acc_filtered = scipy.signal.cwt(acc_filtered.squeeze(), scipy.signal.ricker, [7])
-    acc_filtered4 = scipy.signal.savgol_filter(acc_filtered, 11, 5)
-    acc_filtered = scipy.signal.cwt(acc_filtered4.squeeze(), scipy.signal.ricker, [7])  # See NOTE above
-    acc_filtered = scipy.ndimage.gaussian_filter(
-        acc_filtered.squeeze(), 2
-    )  # NOTE: sigma = windowWidth / 5, windowWidth = 10 (from MATLAB)
-    acc_filtered = scipy.ndimage.gaussian_filter(
-        acc_filtered.squeeze(), 2
-    )  # NOTE: sigma = windowWidth / 5, windowWidth = 10 (from MATLAB)
-    acc_filtered = scipy.ndimage.gaussian_filter(
-        acc_filtered.squeeze(), 3
-    )  # NOTE: sigma = windowWidth / 5, windowWidth = 15 (from MATLAB)
-    acc_filtered = scipy.ndimage.gaussian_filter(
-        acc_filtered.squeeze(), 2
-    )  # NOTE: sigma = windowWidth / 5, windowWidth = 10 (from MATLAB)
-
-    FALLBACK_THRESHOLD = 0.15
-
-    try:
-        active_peak_threshold = find_active_period_peak_threshold(acc_filtered, ALGORITHM_TARGET_SAMPLING_RATE_HZ)
-        signal = acc_filtered
-    except NoActivePeriodsDetectedError:
-        # If we don't find the active periods, use a fallback threshold and use
-        # a less filtered signal for further processing, for which we can better predict the threshold.
-        warnings.warn("No active periods detected, using fallback threshold")
-        active_peak_threshold = FALLBACK_THRESHOLD
-        signal = acc_filtered4
-
-    # Find extrema in signal that might represent steps
-    min_peaks, max_peaks = find_min_max_above_threshold(signal, active_peak_threshold)
-
-    # Combine steps detected by the maxima and minima
-    MIN_N_STEPS = 5
-
-    gs_from_max = find_pulse_trains(max_peaks)
-    gs_from_min = find_pulse_trains(min_peaks)
-    gs_from_max = gs_from_max[gs_from_max[:, 2] >= MIN_N_STEPS]
-    gs_from_min = gs_from_min[gs_from_min[:, 2] >= MIN_N_STEPS]
-
-    combined_final = find_intersections(
-        gs_from_max[:, :2], gs_from_min[:, :2]
-    )  # Combine the gs from the maxima and minima
-
-    if combined_final.size == 0:  # Check if no gs detected
-        return pd.DataFrame(columns=["start", "end"]).astype(int)  # Return empty df, if no gs
-
-    # Find all max_peaks withing each final gait sequence (GS)
-    steps_per_gs = [[x for x in max_peaks if gs[0] <= x <= gs[1]] for gs in combined_final]
-    n_steps_per_gs = np.array([len(steps) for steps in steps_per_gs])
-    mean_step_times = np.array([np.mean(np.diff(steps)) for steps in steps_per_gs])
-
-    # Pad each gs by 0.75*step_time before and after
-    combined_final[:, 0] = np.fix(combined_final[:, 0] - 0.75 * mean_step_times)
-    combined_final[:, 1] = np.fix(combined_final[:, 1] + 0.75 * mean_step_times)
-
-    # Filter again by number of steps, remove any gs with too few steps
-    combined_final = combined_final[n_steps_per_gs >= MIN_N_STEPS]
-
-    if combined_final.size == 0:  # Check if all gs removed
-        return pd.DataFrame(columns=["start", "end"]).astype(int)  # Return empty df if no gs
-
-    # Merge gs if time (in seconds) between consecutive gs is less than MAX_GAP_S
-    MAX_GAP_S = 3
-    combined_final = merge_intervals(combined_final, ALGORITHM_TARGET_SAMPLING_RATE_HZ * MAX_GAP_S)
-
-    # Convert back to original sampling rate
-    combined_final = combined_final * sampling_rate_hz / ALGORITHM_TARGET_SAMPLING_RATE_HZ
-
-    # Cap the start and the end of the signal using clip, incase padding extended any gs past the signal length
-    combined_final = np.clip(combined_final, 0, len(acc))
-
-    return pd.DataFrame(combined_final, columns=["start", "end"]).astype(int)
