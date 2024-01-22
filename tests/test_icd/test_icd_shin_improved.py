451d3df9f1328458512b525e16d5eb9e15407946
dmegaritis <dmgrei3@gmail.com>
2024-01-22 15:43:21 +0000

    test for no ics added

--- tests/test_icd/test_icd_shin_improved.py
+++ tests/test_icd/test_icd_shin_improved.py
@@ -3,7 +3,7 @@ import pandas as pd
 import pytest
 from tpcp.testing import TestAlgorithmMixin
 
-from gaitlink.icd import IcdShinImproved
+from gaitlink.ICD import IcdShinImproved
 
 
 class TestMetaShinImproved(TestAlgorithmMixin):
@@ -22,6 +22,16 @@ class TestShinImprovedUnit:
     def test_invalid_axis_parameter(self):
         with pytest.raises(ValueError):
             IcdShinImproved(axis="invalid").detect(pd.DataFrame(), sampling_rate_hz=100)
+
+
+class TestnoICsDetected:
+    def test_no_ics_detected(self):
+        data = pd.DataFrame(np.zeros((1000, 3)), columns=["acc_x", "acc_y", "acc_z"])
+        output = IcdShinImproved(axis="x")
+        output.detect(data, sampling_rate_hz=40.0)
+        output_ic = output.ic_list_['ic']
+        empty_output = {}
+        assert output_ic.to_dict() == empty_output
 
 
 class TestShinImprovedRegression:
