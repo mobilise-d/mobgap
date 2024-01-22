71098b2262e4fa9cc8fbe53fb5d3e2713d60010b
dmegaritis <dmgrei3@gmail.com>
2024-01-22 15:37:30 +0000

    PyCharm review feedback v2.0

---
+++ tests/test_icd/test_find_zero_crossings.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+import pytest
+from tpcp.testing import TestAlgorithmMixin
+from gaitlink.ICD import find_zero_crossings
+
+
+class Testfind_zero_crossing:
+    __test__ = True
+
+    def test_invalid_mode_parameter(self):
+        with pytest.raises(ValueError):
+            find_zero_crossings(np.ndarray(shape=(1000, 1)), mode="invalid")
