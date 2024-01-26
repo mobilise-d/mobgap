6f6160cda89a520f69c9e126c4c86bd3c71bc8ec
Arne Küderle <a.kuederle@gmail.com>
2024-01-26 15:23:20 +0100

    First pass to align the two IC algos

--- gaitlink/icd/_icd_ionescu.py
+++ gaitlink/icd/_icd_ionescu.py
@@ -2,18 +2,15 @@ from typing import Any, Optional
 
 import numpy as np
 import pandas as pd
+from pywt import cwt
 from scipy.integrate import cumtrapz
-from scipy.signal import cwt, ricker
 from tpcp import cf
 from typing_extensions import Self, Unpack
 
 from gaitlink.consts import GRAV_MS2
-from gaitlink.data_transform._filter import EpflDedriftedGaitFilter
-from gaitlink.data_transform._resample import Resample
+from gaitlink.data_transform import EpflDedriftedGaitFilter, EpflGaitFilter, Resample
 from gaitlink.data_transform.base import BaseFilter, BaseTransformer
 from gaitlink.icd.base import BaseIcdDetector, base_icd_docfiller
-
-# TODO: Some parameter renaming might be in order
 
 
 @base_icd_docfiller
@@ -45,7 +42,7 @@ class IcdIonescu(BaseIcdDetector):
 
     Attributes
     ----------
-    %(icd_list_)s
+    %(ic_list_)s
 
     Notes
     -----
@@ -64,26 +61,26 @@ class IcdIonescu(BaseIcdDetector):
     .. [3] https://github.com/mobilise-d/Mobilise-D-TVS-Recommended-Algorithms/blob/master/ICDA/Library/SD_algo_AMC.m
     """
 
-    # ALL PARAMETERS THAT ARE NOT DATA-RELATED IN THE INIT METHOD
-    pre_filter: Optional[BaseFilter]  # IS IT RIGHT? --> Optional[EpflDedriftedGaitFilter]
+    pre_filter: Optional[BaseFilter]
     resampler: Optional[BaseTransformer]
-    target_sampling_rate_hz: float
+
+    _INTERNAL_FILTER_SAMPLING_RATE_HZ: int = 40
 
     def __init__(
-        self, *, pre_filter: Optional[BaseFilter] = cf(EpflDedriftedGaitFilter()), cwt_width: float = 6.4
+        self, *, pre_filter: Optional[BaseFilter] = cf(EpflDedriftedGaitFilter()), cwt_width: float = 9.0
     ) -> None:
         self.pre_filter = pre_filter
         self.cwt_width = cwt_width
 
     @base_icd_docfiller
     def detect(self, data: pd.DataFrame, *, sampling_rate_hz: float, **_: Unpack[dict[str, Any]]) -> Self:
-        """%(detect_short)s. It is a summary of the detect method.
+        """%(detect_short)s.
 
         Parameters
         ----------
-        %(detect_para)s # input parameters
+        %(detect_para)s
 
-        %(detect_return)s # output parameters
+        %(detect_return)s
 
         """
         self.data = data
@@ -97,44 +94,49 @@ class IcdIonescu(BaseIcdDetector):
         acc_v = acc_v / GRAV_MS2  # GRAV_MS2 = 9.81: [m/s^2]--> [g]
 
         # 1. RESAMPLING
-        target_sampling_rate_hz = 40
-        # Create an instance of the Resample class with the target sampling rate
-        resampler = Resample(target_sampling_rate_hz)
-        # Perform the resampling operation by calling the transform method
-        resampled = resampler.transform(acc_v, sampling_rate_hz=sampling_rate_hz)
-        acc_v_40 = resampled.transformed_data_
+        signal_downsampled = (
+            Resample(self._INTERNAL_FILTER_SAMPLING_RATE_HZ)
+            .transform(data=acc_v, sampling_rate_hz=sampling_rate_hz)
+            .transformed_data_
+        )
         # 2. BAND-PASS FILTERING
         # Padding for short data: this is required because the length of the input vector must be greater
         # than padlen argument of the filtfilt function (3*max(b,a) = 306 by deafult), otherwise a
         # ValueError is raised.
-        len_padd = 4 * 102  # len(b) = 102.
-        acc_v_40_zp = np.pad(acc_v_40.squeeze(), (len_padd, len_padd), "wrap")
+        n_coefficients = len(EpflGaitFilter().coefficients[0])
+        len_pad = 4 * n_coefficients
+        signal_downsampled_padded = np.pad(signal_downsampled, (len_pad, len_pad), "wrap")
         # The clone() method is used in order to ensure that the new second instance is independent
         acc_v_40_bpf = (
             self.pre_filter.clone()
-            .filter(acc_v_40_zp, sampling_rate_hz=target_sampling_rate_hz)
+            .filter(signal_downsampled_padded, sampling_rate_hz=self._INTERNAL_FILTER_SAMPLING_RATE_HZ)
             .filtered_data_.squeeze()
         )
         # Remove the padding
-        acc_v_40_bpf_rmzp = acc_v_40_bpf[len_padd - 1 : -len_padd]
+        acc_v_40_bpf_rmzp = acc_v_40_bpf[len_pad - 1 : -len_pad]
         # 3. CUMULATIVE INTEGRAL
-        acc_v_lp_int = cumtrapz(acc_v_40_bpf_rmzp, initial=0) / target_sampling_rate_hz
+        acc_v_lp_int = cumtrapz(acc_v_40_bpf_rmzp, initial=0) / self._INTERNAL_FILTER_SAMPLING_RATE_HZ
         # 4. CONTINUOUS WAVELET TRANSFORM (CWT)
-        acc_v_lp_int_cwt = cwt(acc_v_lp_int.squeeze(), ricker, [self.cwt_width])
+        acc_v_lp_int_cwt = cwt(
+            acc_v_lp_int.squeeze(),
+            [self.cwt_width],
+            "gaus2",
+            sampling_period=1 / self._INTERNAL_FILTER_SAMPLING_RATE_HZ,
+        )
         # Remove the mean from accVLPIntCwt
         acc_v_lp_int_cwt = acc_v_lp_int_cwt - acc_v_lp_int_cwt.mean()
         # 5. INTRA-ZERO-CROSSINGS PEAK DETECTION
-        x = acc_v_lp_int_cwt.squeeze()  # squeeze extra dimensions
+        x = acc_v_lp_int_cwt.squeeze()
         # Detect the maximum peaks between the zero crossings
         pks, ipks = max_peaks_between_zc(x)
         # Filter negative peaks
         icd_array = np.array(ipks)[pks < 0]
         # Consider indices of the first and last elements of the input dataframe as ICs
         # icd_array = np.concatenate([np.array([1]),icd_array,np.array([data.shape[0]])])
-        # Convert to seconds.
-        icd_array = (icd_array + 1) / target_sampling_rate_hz
+        # TODO: Do we need the +1 here?
+        icd_array = (icd_array + 1) / self._INTERNAL_FILTER_SAMPLING_RATE_HZ
         # OUTPUT: first and last element of the gsd are considered ICs by default
-        self.icd_list_ = pd.Series(data=icd_array, name="ic")
+        self.icd_list_ = pd.DataFrame({"ic": icd_array}).rename_axis(index="ic_id")
         return self
 
 
@@ -142,11 +144,12 @@ def max_peaks_between_zc(x: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
     # Peaks and their locations between zero-crossings of array x
     #
     # Find zero crossing locations
+    # TODO: This might not handle cases well, where the value is exactly zero
     ix = np.asarray(np.abs(np.diff(np.sign(x))) == 2).nonzero()[0] + 1
     n_zcs = len(ix) - 1  # Number of zero-crossings minus 1
     # Find the indices of maximum values between zero crossings. Code considers L separate intervals between the L+1
     # zero crossings and returns the index of the peak in each interval
-    ipks = [np.argmax(np.abs(x[ix[i] : ix[i + 1]])) + ix[i] for i in range(n_zcs)]
+    ipks = np.array([np.argmax(np.abs(x[ix[i] : ix[i + 1]])) + ix[i] for i in range(n_zcs)])
     # Get the signed peaks
     pks = x[ipks]
     return pks, ipks
