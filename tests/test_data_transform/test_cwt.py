5170827776b9d37866e1b49dc23d89715132edf9
Arne Küderle <a.kuederle@gmail.com>
2024-02-01 13:37:12 +0100

    Switched to pywt for cwt to avoid deprecation in scipy

--- tests/test_data_transform/test_cwt.py
+++ tests/test_data_transform/test_cwt.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pytest
-from scipy.signal import cwt, ricker
+from pywt import cwt
 from tpcp.testing import TestAlgorithmMixin
 
 from gaitlink.data_transform._cwt import CwtFilter
@@ -19,26 +19,26 @@ class TestMetaCwtFilter(TestAlgorithmMixin):
         # Use some random input data for the meta-test (actual values might not be important)
         input_data = np.array([1.0, 2.0, 3.0, 4.0])
         # Perform the Continuous Wavelet Transform using your CwtFilter class
-        result = cwt_filter_instance.transform(input_data)
+        result = cwt_filter_instance.transform(input_data, sampling_rate_hz=100)
 
         # Return the CwtFilter class instance with initial conditions
         return result
 
 
 class TestCwtFilter:
-    def test_compare_against_scipy(self):
-        wavelet = ricker
+    def test_compare_against_pywt(self):
+        wavelet = "gaus2"
         width = 10
 
         cwt_filter = CwtFilter(wavelet=wavelet, width=width)
 
         data = np.random.random((1000, 2))
 
-        cwt_filter.transform(data)
+        cwt_filter.filter(data, sampling_rate_hz=100)
 
         # Compare to calling cwt directly
-        first_col = cwt(data[:, 0], wavelet, [width])
-        second_col = cwt(data[:, 1], wavelet, [width])
+        first_col, _ = cwt(data[:, 0], [width], wavelet, sampling_period=1 / 100)
+        second_col, _ = cwt(data[:, 1], [width], wavelet, sampling_period=1 / 100)
 
         np.testing.assert_equal(cwt_filter.transformed_data_[:, 0], first_col.flatten())
         np.testing.assert_equal(cwt_filter.transformed_data_[:, 1], second_col.flatten())
