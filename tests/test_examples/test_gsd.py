4e55e29f69571473c54dcfac9ba203a514a4d7c1
Annika Mücke <annika-muecke@outlook.de>
2024-02-01 10:36:27 +0100

    adapt example

--- tests/test_examples/test_gsd.py
+++ tests/test_examples/test_gsd.py
@@ -1,16 +1,21 @@
+import numpy as np
+
+
 def test_gsd_iluz(snapshot):
     from examples.gsd._01_gsd_iluz import (
         categorized_intervals,
         long_trial_output,
         long_trial_output_modified,
         matches,
+        metrics_all,
+        prec_rec_f1_dict,
         short_trial_output,
     )
 
     snapshot.assert_match(long_trial_output.gs_list_, "long_trial_output")
     snapshot.assert_match(long_trial_output_modified.gs_list_, "long_trial_output_modified")
     snapshot.assert_match(short_trial_output.gs_list_, "short_trial_output")
-    snapshot.assert_match(categorized_intervals.tp_intervals, "tp_intervals")
-    snapshot.assert_match(categorized_intervals.fp_intervals, "fp_intervals")
-    snapshot.assert_match(categorized_intervals.fn_intervals, "fn_intervals")
+    snapshot.assert_match(categorized_intervals, "categorized_intervals")
+    snapshot.assert_match(np.array(list(prec_rec_f1_dict.values())), "prec_rec_f1_values")
+    snapshot.assert_match(metrics_all, "metrics_all")
     snapshot.assert_match(matches, "matches")
