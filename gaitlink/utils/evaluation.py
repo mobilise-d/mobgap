d20337b87e16b47dcf4b1f17913a5101afd0bc46
Annika Mücke <annika-muecke@outlook.de>
2024-02-01 14:22:25 +0100

    add evaluation utils and tests (adapted from gaitmap)

---
+++ gaitlink/utils/evaluation.py
@@ -0,0 +1,346 @@
+"""Module containing functions to calculate common performance metrics."""
+import warnings
+from typing import Literal
+
+import pandas as pd
+
+__all__ = [
+    "precision_recall_f1_score",
+    "precision_score",
+    "recall_score",
+    "specificity_score",
+    "accuracy_score",
+    "npv_score",
+    "f1_score",
+    "count_samples_in_match_intervals",
+    "count_samples_in_intervals",
+]
+
+
+def precision_recall_f1_score(
+    matches_df: pd.DataFrame, *, zero_division: Literal["warn", 0, 1] = "warn"
+) -> dict[str, float]:
+    """Compute precision, recall and F1-score.
+
+    The precision is the ratio tp / (tp + fp), where tp is the number of true positives and fp the number of false
+    positives.
+    Intuitively speaking, the precision is the ability of the classifier not to label a negative sample as positive.
+
+    The recall is the ratio tp / (tp + fn), where tp is the number of true positives and fn the number of false
+    negatives.
+    Intuitively speaking, the recall is the ability of the classifier to find all the positive samples.
+
+    The F1 score can be interpreted as the harmonic mean of precision and recall, where an F1 score reaches its
+    best value at 1 and worst score at 0.
+
+    Parameters
+    ----------
+    matches_df
+        A 3 column dataframe.
+        # TODO: Describe the dataframe structure depending on the input type.
+        The `match_type` column indicates the type of match:
+        "tp" (true positive), "fp" (false positives) or "fn" (false negative) if no segmented
+        counterpart exists.
+    zero_division : "warn", 0 or 1, default="warn"
+        Sets the value to return when there is a zero division. If set to
+        "warn", this acts as 0, but warnings are also raised.
+
+    Returns
+    -------
+    score_metrics : {"precision": precision, "recall": recall, "f1_score": f1_score}
+    """
+    precisions = precision_score(matches_df.copy(), zero_division=zero_division)
+    recalls = recall_score(matches_df.copy(), zero_division=zero_division)
+    f1_scores = f1_score(matches_df.copy(), zero_division=zero_division)
+
+    return {
+        "precision": precisions,
+        "recall": recalls,
+        "f1_score": f1_scores,
+    }
+
+
+def precision_score(matches_df: pd.DataFrame, *, zero_division: Literal["warn", 0, 1] = "warn") -> float:
+    """Compute the precision.
+
+    The precision is the ratio tp / (tp + fp) where tp is the number of true positives and fp the number of false
+    positives.
+    Intuitively speaking, the precision is the ability of the classifier not to label a negative sample as positive.
+
+    The best value is 1 and the worst value is 0.
+    Precision can be also referred to as positive predictive value (PPV).
+
+    Parameters
+    ----------
+    matches_df
+        A 3 column dataframe.
+        # TODO: Describe the dataframe structure depending on the input type.
+        The `match_type` column indicates the type of match:
+        "tp" (true positive), "fp" (false positives) or "fn" (false negative) if no segmented
+        counterpart exists.
+    zero_division : "warn", 0 or 1, default="warn"
+        Sets the value to return when there is a zero division. If set to
+        "warn", this acts as 0, but warnings are also raised.
+
+    Returns
+    -------
+    precision_score: float
+        Value between 0 and 1.
+    """
+    if _input_is_gsd_matches_df(matches_df):
+        tp = count_samples_in_match_intervals(matches_df, "tp")
+        fp = count_samples_in_match_intervals(matches_df, "fp")
+    else:
+        raise NotImplementedError("Only GSDs are supported as input at the moment.")
+
+    output = _calculate_score(tp, tp + fp, zero_division=zero_division, caller_function_name="precision")
+
+    return output
+
+
+def recall_score(matches_df: pd.DataFrame, *, zero_division: Literal["warn", 0, 1] = "warn") -> float:
+    """Compute the recall.
+
+    The recall is the ratio tp / (tp + fn) where tp is the number of true positives and fn the number of false
+    negatives.
+    Intuitively speaking, the recall is the ability of the classifier to find all the positive samples.
+
+    The best value is 1 and the worst value is 0.
+    Recall can be also referred to as sensitivity, hit rate, or true positive rate (TPR).
+
+    Parameters
+    ----------
+    matches_df
+        A 3 column dataframe.
+        # TODO: Describe the dataframe structure depending on the input type.
+        The `match_type` column indicates the type of match:
+        "tp" (true positive), "fp" (false positives) or "fn" (false negative) if no segmented
+        counterpart exists.
+    zero_division : "warn", 0 or 1, default="warn"
+        Sets the value to return when there is a zero division. If set to
+        "warn", this acts as 0, but warnings are also raised.
+
+    Returns
+    -------
+    recall_score: float
+        Value between 0 and 1.
+    """
+    if _input_is_gsd_matches_df(matches_df):
+        tp = count_samples_in_match_intervals(matches_df, "tp")
+        fn = count_samples_in_match_intervals(matches_df, "fn")
+    else:
+        raise NotImplementedError("Only GSDs are supported as input at the moment.")
+
+    output = _calculate_score(tp, tp + fn, zero_division=zero_division, caller_function_name="recall")
+
+    return output
+
+
+# TODO: at which point should the user specify the recording length/tn for the metrics requiring it?
+def specificity_score(matches_df: pd.DataFrame, tn: int, *, zero_division: Literal["warn", 0, 1] = "warn") -> float:
+    """Compute the specificity.
+
+    The specificity is the ratio tn / (tn + fp) where tn is the number of true negatives and fp the number of false
+    positives.
+    Intuitively speaking, the specificity is the ability of the classifier to find all the negative samples.
+
+    The best value is 1 and the worst value is 0.
+    Specificity can be also referred to as the true negative rate (TNR).
+
+    Parameters
+    ----------
+    matches_df
+        A 3 column dataframe.
+        # TODO: Describe the dataframe structure depending on the input type.
+        The `match_type` column indicates the type of match:
+        "tp" (true positive), "fp" (false positives) or "fn" (false negative) if no segmented
+        counterpart exists.
+    tn: int
+        Number of true negatives. Needs to be provided as an argument, as it can not be inferred from the matches_df.
+    zero_division : "warn", 0 or 1, default="warn"
+        Sets the value to return when there is a zero division. If set to
+        "warn", this acts as 0, but warnings are also raised.
+
+    Returns
+    -------
+    specificity_score: float
+        Value between 0 and 1.
+    """
+    if _input_is_gsd_matches_df(matches_df):
+        fp = count_samples_in_match_intervals(matches_df, "fp")
+    else:
+        raise NotImplementedError("Only GSDs are supported as input at the moment.")
+
+    output = _calculate_score(tn, tn + fp, zero_division=zero_division, caller_function_name="specificity")
+
+    return output
+
+
+def accuracy_score(matches_df: pd.DataFrame, tn: int, *, zero_division: Literal["warn", 0, 1] = "warn") -> float:
+    """Compute the specificity.
+
+    The accuracy is the ratio (tp + tn) / (fp + fn + tp + tn) where tp is the number of true positives,
+    tn is the number of true negatives, fp the number of false positives and fn the number of false negatives.
+    Intuitively speaking, the accuracy is ratio of correctly detected positives out of all samples.
+
+    The best value is 1 and the worst value is 0.
+
+    Parameters
+    ----------
+    matches_df
+        A 3 column dataframe.
+        # TODO: Describe the dataframe structure depending on the input type.
+        The `match_type` column indicates the type of match:
+        "tp" (true positive), "fp" (false positives) or "fn" (false negative) if no segmented
+        counterpart exists.
+    tn: int
+        Number of true negatives. Needs to be provided as an argument, as it can not be inferred from the matches_df.
+    zero_division : "warn", 0 or 1, default="warn"
+        Sets the value to return when there is a zero division. If set to
+        "warn", this acts as 0, but warnings are also raised.
+
+    Returns
+    -------
+    accuracy_score: float
+        Value between 0 and 1.
+    """
+    if _input_is_gsd_matches_df(matches_df):
+        tp = count_samples_in_match_intervals(matches_df, "tp")
+        fn = count_samples_in_match_intervals(matches_df, "fn")
+        fp = count_samples_in_match_intervals(matches_df, "fp")
+    else:
+        raise NotImplementedError("Only GSDs are supported as input at the moment.")
+
+    output = _calculate_score(tn + tp, fp + fn + tp + tn, zero_division=zero_division, caller_function_name="accuracy")
+
+    return output
+
+
+def npv_score(matches_df: pd.DataFrame, tn: int, *, zero_division: Literal["warn", 0, 1] = "warn") -> float:
+    """Compute the negative predictive value (NPV).
+
+    The NPV is the ratio tn / (tn + fn) where tn is the number of true negatives and fn the number of false
+    negatives.
+    Intuitively speaking, the NPV is the ability of the classifier not to label a positive sample as negative.
+
+    The best value is 1 and the worst value is 0.
+
+    Parameters
+    ----------
+    matches_df
+        A 3 column dataframe.
+        # TODO: Describe the dataframe structure depending on the input type.
+        The `match_type` column indicates the type of match:
+        "tp" (true positive), "fp" (false positives) or "fn" (false negative) if no segmented
+        counterpart exists.
+    tn: int
+        Number of true negatives. Needs to be provided as an argument, as it can not be inferred from the matches_df.
+    zero_division : "warn", 0 or 1, default="warn"
+        Sets the value to return when there is a zero division. If set to
+        "warn", this acts as 0, but warnings are also raised.
+
+    Returns
+    -------
+    npv_score: float
+        Value between 0 and 1.
+    """
+    if _input_is_gsd_matches_df(matches_df):
+        fn = count_samples_in_match_intervals(matches_df, "fn")
+    else:
+        raise NotImplementedError("Only GSDs are supported as input at the moment.")
+
+    output = _calculate_score(tn, tn + fn, zero_division=zero_division, caller_function_name="npv")
+
+    return output
+
+
+def f1_score(matches_df: pd.DataFrame, *, zero_division: Literal["warn", 0, 1] = "warn") -> float:
+    """Compute the F1 score, also known as balanced F-score or F-measure.
+
+    The F1 score can be interpreted as the harmonic mean of precision and recall, where an F1 score reaches its
+    best value at 1 and worst score at 0.
+
+    The formula for the F1 score is:
+    F1 = 2 * (precision * recall) / (precision + recall)
+
+    Parameters
+    ----------
+    matches_df
+        A 3 column dataframe.
+        # TODO: Describe the dataframe structure depending on the input type.
+        The `match_type` column indicates the type of match:
+        "tp" (true positive), "fp" (false positives) or "fn" (false negative) if no segmented
+        counterpart exists.
+    zero_division : "warn", 0 or 1, default="warn"
+        Sets the value to return when there is a zero division. If set to
+        "warn", this acts as 0, but warnings are also raised.
+
+    Returns
+    -------
+    f1_score: float
+        Value between 0 and 1.
+    """
+    recall = recall_score(matches_df.copy())
+    precision = precision_score(matches_df.copy())
+    output = _calculate_score(
+        2 * (precision * recall),
+        precision + recall,
+        zero_division=zero_division,
+        caller_function_name="f1",
+    )
+
+    return output
+
+
+def count_samples_in_match_intervals(match_df: pd.DataFrame, match_type: Literal["tp", "fp", "fn"]) -> int:
+    """Count the number of samples in the intervals of the given match type.
+
+    Parameters
+    ----------
+    match_df: pd.DataFrame
+        A DataFrame containing the categorized intervals with their `start` and `end` index and the respective match
+        type in a column named `match_type`.
+    match_type: Literal["tp", "fp", "fn"]
+        The match type to count the samples for.
+    """
+    try:
+        _ = match_df[["start", "end", "match_type"]]
+    except KeyError as e:
+        raise ValueError("`match_df` must have columns named 'start', 'end' and 'match_type'.") from e
+    matches = match_df.query(f"match_type == '{match_type}'")
+    return count_samples_in_intervals(matches)
+
+
+def count_samples_in_intervals(intervals: pd.DataFrame) -> int:
+    """Count the number of samples in the given interval list."""
+    try:
+        _ = intervals[["start", "end"]]
+    except KeyError as e:
+        raise ValueError("`intervals` must have columns named 'start' and 'end'.") from e
+    return int((intervals["end"] - intervals["start"]).sum())
+
+
+def _calculate_score(a: float, b: float, *, zero_division: Literal["warn", 0, 1], caller_function_name: str) -> float:
+    try:
+        return a / b
+    except ZeroDivisionError as e:
+        if zero_division == "warn":
+            warnings.warn(
+                f"Zero division happened while calculating the {caller_function_name} score. Returning 0",
+                UserWarning,
+                stacklevel=2,
+            )
+            return 0
+
+        if zero_division in [0, 1]:
+            return zero_division
+
+        raise ValueError('"zero_division" must be set to "warn", 0 or 1!') from e
+
+
+def _input_is_gsd_matches_df(matches_df: pd.DataFrame) -> bool:
+    try:
+        _ = matches_df[["start", "end", "match_type"]]
+    except KeyError:
+        return False
+    return True
---
+++ tests/test_utils/test_evaluation.py
@@ -0,0 +1,239 @@
+import numpy as np
+import pandas as pd
+import pytest
+from numpy.testing import assert_array_equal
+
+from gaitlink.utils.evaluation import (
+    accuracy_score,
+    f1_score,
+    npv_score,
+    precision_recall_f1_score,
+    precision_score,
+    recall_score,
+    specificity_score,
+)
+
+
+def _create_dummy_matches_df(num_tp, num_fp, num_fn):
+    tp_df = pd.DataFrame(
+        np.column_stack([np.repeat(0, num_tp), np.repeat(1, num_tp), np.repeat("tp", num_tp)]),
+        columns=["start", "end", "match_type"],
+    )
+    tp_df[["start", "end"]] = tp_df[["start", "end"]].astype(int)
+    fp_df = pd.DataFrame(
+        np.column_stack([np.repeat(0, num_fp), np.repeat(1, num_fp), np.repeat("fp", num_fp)]),
+        columns=["start", "end", "match_type"],
+    )
+    fp_df[["start", "end"]] = fp_df[["start", "end"]].astype(int)
+    fn_df = pd.DataFrame(
+        np.column_stack([np.repeat(0, num_fn), np.repeat(1, num_fn), np.repeat("fn", num_fn)]),
+        columns=["start", "end", "match_type"],
+    )
+    fn_df[["start", "end"]] = fn_df[["start", "end"]].astype(int)
+
+    return pd.concat([tp_df, fp_df, fn_df])
+
+
+class TestEvaluationScores:
+    def test_precision(self):
+        matches_df = _create_dummy_matches_df(5, 5, 5)
+
+        precision = precision_score(matches_df)
+
+        assert_array_equal(precision, 0.5)
+
+    def test_perfect_precision(self):
+        matches_df = _create_dummy_matches_df(5, 0, 5)
+
+        precision = precision_score(matches_df)
+
+        assert_array_equal(precision, 1.0)
+
+    def test_recall(self):
+        matches_df = _create_dummy_matches_df(5, 5, 5)
+
+        recall = recall_score(matches_df)
+
+        assert_array_equal(recall, 0.5)
+
+    def test_perfect_recall(self):
+        matches_df = _create_dummy_matches_df(5, 5, 0)
+
+        recall = recall_score(matches_df)
+
+        assert_array_equal(recall, 1.0)
+
+    def test_f1_score(self):
+        matches_df = _create_dummy_matches_df(5, 5, 5)
+
+        f1 = f1_score(matches_df)
+
+        assert_array_equal(f1, 0.5)
+
+    def test_perfect_f1_score(self):
+        matches_df = _create_dummy_matches_df(5, 0, 0)
+
+        f1 = f1_score(matches_df)
+
+        assert_array_equal(f1, 1.0)
+
+    def test_precision_recall_f1(self):
+        matches_df = _create_dummy_matches_df(5, 5, 5)
+
+        eval_metrics = precision_recall_f1_score(matches_df)
+
+        assert_array_equal(list(eval_metrics.values()), [0.5, 0.5, 0.5])
+
+    def test_perfect_precision_recall_f1(self):
+        matches_df = _create_dummy_matches_df(5, 0, 0)
+
+        eval_metrics = precision_recall_f1_score(matches_df)
+
+        assert_array_equal(list(eval_metrics.values()), [1.0, 1.0, 1.0])
+
+    def test_specificity(self):
+        matches_df = _create_dummy_matches_df(5, 5, 5)
+
+        specificity = specificity_score(matches_df, 5)
+
+        assert_array_equal(specificity, 0.5)
+
+    def test_perfect_specificity(self):
+        matches_df = _create_dummy_matches_df(0, 5, 5)
+
+        specificity = specificity_score(matches_df, 0)
+
+        assert_array_equal(specificity, 1.0)
+
+    def test_accuracy(self):
+        matches_df = _create_dummy_matches_df(5, 5, 5)
+
+        accuracy = accuracy_score(matches_df, 5)
+
+        assert_array_equal(accuracy, 0.5)
+
+    def test_perfect_accuracy(self):
+        matches_df = _create_dummy_matches_df(5, 0, 0)
+
+        accuracy = accuracy_score(matches_df, 5)
+
+        assert_array_equal(accuracy, 1.0)
+
+    def test_npv_score(self):
+        matches_df = _create_dummy_matches_df(5, 5, 5)
+
+        npv = npv_score(matches_df, 5)
+
+        assert_array_equal(npv, 0.5)
+
+    def test_perfect_npv_score(self):
+        matches_df = _create_dummy_matches_df(5, 5, 0)
+
+        npv = npv_score(matches_df, 5)
+
+        assert_array_equal(npv, 1.0)
+
+
+class TestDivisionByZeroReturn:
+    @pytest.fixture(
+        autouse=True,
+        params=(
+            [precision_score, [0, 0, 5], None, "warn", 0],
+            [precision_score, [0, 0, 5], None, 0, 0],
+            [precision_score, [0, 0, 5], None, 1, 1],
+            [recall_score, [0, 5, 0], None, "warn", 0],
+            [recall_score, [0, 5, 0], None, 0, 0],
+            [recall_score, [0, 5, 0], None, 1, 1],
+            [f1_score, [0, 0, 0], None, "warn", 0],
+            [f1_score, [0, 0, 0], None, 0, 0],
+            [f1_score, [0, 0, 0], None, 1, 1],
+            [precision_recall_f1_score, [0, 0, 0], None, "warn", 0],
+            [precision_recall_f1_score, [0, 0, 0], None, 0, 0],
+            [precision_recall_f1_score, [0, 0, 0], None, 1, 1],
+            [specificity_score, [0, 0, 5], 0, "warn", 0],
+            [specificity_score, [0, 0, 5], 0, 0, 0],
+            [specificity_score, [0, 0, 5], 0, 1, 1],
+            [accuracy_score, [0, 0, 0], 0, "warn", 0],
+            [accuracy_score, [0, 0, 0], 0, 0, 0],
+            [accuracy_score, [0, 0, 0], 0, 1, 1],
+            [npv_score, [0, 5, 0], 0, "warn", 0],
+            [npv_score, [0, 5, 0], 0, 0, 0],
+            [npv_score, [0, 5, 0], 0, 1, 1],
+        ),
+    )
+    def make_methods(self, request):
+        self.func, self.arguments, self.tn, self.zero_division, self.expected_output = request.param
+
+    def test_division_by_zero_return(self):
+        matches_df = _create_dummy_matches_df(*self.arguments)
+
+        if self.tn is not None:
+            eval_metrics = self.func(matches_df, self.tn, zero_division=self.zero_division)
+        else:
+            eval_metrics = self.func(matches_df, zero_division=self.zero_division)
+
+        assert_array_equal(
+            np.array(list(eval_metrics.values()) if isinstance(eval_metrics, dict) else eval_metrics),
+            self.expected_output,
+        )
+
+
+class TestDivisionByZeroWarnings:
+    @pytest.fixture(
+        autouse=True,
+        params=(
+            [precision_score, [0, 0, 0], None, "warn", "calculating the precision score"],
+            [recall_score, [0, 5, 0], None, "warn", "calculating the recall score"],
+            [f1_score, [0, 0, 0], None, "warn", "calculating the f1 score"],
+            [precision_recall_f1_score, [0, 0, 0], None, "warn", "calculating the f1 score"],
+            [specificity_score, [0, 0, 5], 0, "warn", "calculating the specificity score"],
+            [accuracy_score, [0, 0, 0], 0, "warn", "calculating the accuracy score"],
+            [npv_score, [0, 5, 0], 0, "warn", "calculating the npv score"],
+        ),
+    )
+    def make_methods(self, request):
+        self.func, self.arguments, self.tn, self.zero_division, self.warning_message = request.param
+
+    def test_division_by_zero_warnings(self):
+        with pytest.warns(UserWarning) as w:
+            if self.tn is not None:
+                self.func(_create_dummy_matches_df(*self.arguments), self.tn, zero_division=self.zero_division)
+            else:
+                self.func(_create_dummy_matches_df(*self.arguments), zero_division=self.zero_division)
+
+        # check that the message matches
+        assert self.warning_message in w[-1].message.args[0]
+
+
+class TestDivisionByZeroError:
+    @pytest.fixture(
+        autouse=True,
+        params=(
+            [precision_score, [0, 0, 5], None, ""],
+            [precision_score, [0, 0, 5], None, 2],
+            [recall_score, [0, 5, 0], None, ""],
+            [recall_score, [0, 5, 0], None, 2],
+            [f1_score, [0, 0, 0], None, ""],
+            [f1_score, [0, 0, 0], None, 2],
+            [precision_recall_f1_score, [0, 0, 0], None, ""],
+            [precision_recall_f1_score, [0, 0, 0], None, 2],
+            [specificity_score, [0, 0, 5], 0, ""],
+            [specificity_score, [0, 0, 5], 0, 2],
+            [accuracy_score, [0, 0, 0], 0, ""],
+            [accuracy_score, [0, 0, 0], 0, 2],
+            [npv_score, [0, 5, 0], 0, ""],
+            [npv_score, [0, 5, 0], 0, 2],
+        ),
+    )
+    def make_methods(self, request):
+        self.func, self.arguments, self.tn, self.zero_division = request.param
+
+    def test_division_by_zero_warnings(self):
+        with pytest.raises(ValueError) as e:
+            if self.tn is not None:
+                self.func(_create_dummy_matches_df(*self.arguments), self.tn, zero_division=self.zero_division)
+            else:
+                self.func(_create_dummy_matches_df(*self.arguments), zero_division=self.zero_division)
+
+        # check that the message matches
+        assert str(e.value) == '"zero_division" must be set to "warn", 0 or 1!'
