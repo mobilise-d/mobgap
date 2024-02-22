2625751a2a99806fa273cd2bc3f4c2862e1c1eb8
Arne Küderle <a.kuederle@gmail.com>
2024-02-22 16:31:21 +0100

    Better tests for new gs-iterator features and added option to resample cad

--- tests/test_pipeline/test_gait_sequence_iterator.py
+++ tests/test_pipeline/test_gait_sequence_iterator.py
@@ -5,6 +5,7 @@ import pytest
 from pandas.testing import assert_frame_equal
 
 from gaitlink.pipeline import GsIterator, create_aggregate_df, iter_gs
+from gaitlink.pipeline._gs_iterator import GaitSequence, WalkingBout
 
 
 class TestGsIterationFunc:
@@ -21,6 +22,24 @@ class TestGsIterationFunc:
         second = next(iterator)
         assert second[0] == ("s2", 5, 10)
         assert_frame_equal(second[1], pd.DataFrame({"data": [6, 7, 8, 9, 10]}, index=[5, 6, 7, 8, 9]))
+
+    @pytest.mark.parametrize("col_name", ["wb_id", "gs_id"])
+    @pytest.mark.parametrize("as_index", [True, False])
+    def test_correct_dtype(self, col_name, as_index):
+        dummy_data = pd.DataFrame({"data": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]})
+        dummy_sections = pd.DataFrame({"start": [0, 5], "end": [5, 10], col_name: ["s1", "s2"]})
+        if as_index:
+            dummy_sections = dummy_sections.set_index(col_name)
+
+        iterator = iter_gs(dummy_data, dummy_sections)
+
+        first = next(iterator)[0]
+        if col_name == "wb_id":
+            assert isinstance(first, WalkingBout)
+            assert first.wb_id == first.id == "s1"
+        else:
+            assert isinstance(first, GaitSequence)
+            assert first.gs_id == first.id == "s1"
 
 
 class TestGsIterator:
@@ -52,7 +71,10 @@ class TestGsIterator:
         inputs, input_dfs = zip(*iterator.inputs_)
         assert inputs == (("s1", 0, 5), ("s2", 5, 10))
         assert_frame_equal(input_dfs[0], pd.DataFrame({"data": [1, 2, 3, 4, 5]}, index=[0, 1, 2, 3, 4]))
-        assert_frame_equal(input_dfs[1], pd.DataFrame({"data": [6, 7, 8, 9, 10]}, index=[5, 6, 7, 8, 9]))
+        assert_frame_equal(
+            input_dfs[1],
+            pd.DataFrame({"data": [6, 7, 8, 9, 10]}, index=[5, 6, 7, 8, 9]),
+        )
 
     def test_agg(self):
         @dataclass
@@ -81,13 +103,16 @@ class TestGsIterator:
         for (s, d), r in iterator.iterate(dummy_data, dummy_sections):
             # We set the values "relative" to the start of the section, but expect the aggregation to make the values
             # relative to the start of the recording.
-            r.ic_list = pd.DataFrame({"ic": [0, s.end - s.start]}).rename_axis("s_id")
+            r.ic_list = pd.DataFrame({"ic": [0, s.end - s.start]}).rename_axis("ic_id")
 
         assert_frame_equal(
             iterator.results_.ic_list,
             pd.DataFrame(
                 {"ic": [0, 5, 5, 10]},
-                index=pd.MultiIndex.from_tuples([("s1", 0), ("s1", 1), ("s2", 0), ("s2", 1)], names=["wb_id", "s_id"]),
+                index=pd.MultiIndex.from_tuples(
+                    [("s1", 0), ("s1", 1), ("s2", 0), ("s2", 1)],
+                    names=["wb_id", "ic_id"],
+                ),
             ),
         )
 
@@ -119,6 +144,37 @@ class TestAggregateDf:
             pd.DataFrame(
                 {"start": expected_start, "end": expected_end},
                 index=pd.MultiIndex.from_product((example_sequences.index, [0, 1]), names=["wb_id", None]),
+            ),
+        )
+
+    @pytest.mark.parametrize("fix_index_offset", [True, False])
+    def test_index_offset(self, fix_index_offset):
+        example_sequences = pd.DataFrame({"start": [1, 5], "end": [5, 10]}, index=["s1", "s2"]).rename_axis(
+            index="wb_id"
+        )
+        example_inputs = [(s, pd.DataFrame()) for s in example_sequences.reset_index().itertuples(index=False)]
+
+        example_results = [
+            pd.DataFrame({"value": [1, 3]}, index=[1, 2]),
+            pd.DataFrame({"value": [2, 4]}, index=[3, 4]),
+        ]
+
+        aggregate_df = create_aggregate_df(fix_gs_offset_index=fix_index_offset)
+        aggregated = aggregate_df(example_inputs, example_results)
+
+        if fix_index_offset:
+            expected_index = [2, 3, 8, 9]
+        else:
+            expected_index = [1, 2, 3, 4]
+
+        expected_index = pd.MultiIndex.from_tuples(
+            zip(*(["s1", "s1", "s2", "s2"], expected_index)), names=["wb_id", None]
+        )
+        assert_frame_equal(
+            aggregated,
+            pd.DataFrame(
+                {"value": [1, 3, 2, 4]},
+                index=expected_index,
             ),
         )
 
