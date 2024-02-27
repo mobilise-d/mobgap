f77e92c8c828b4a93d5bbda31a3bdf9a18c379e5
Annika Mücke <annika-muecke@outlook.de>
2024-02-27 14:02:48 +0100

    add performance evaluation to example

--- tests/test_examples/test_icd.py
+++ tests/test_examples/test_icd.py
@@ -1,13 +1,17 @@
 # This is needed to avoid plots to open
 import matplotlib
+import pandas as pd
 
 matplotlib.use("Agg")
 
 
 def test_icd_ionescu(snapshot):
-    from examples.icd._01_icd_ionescu import detected_ics
+    from examples.icd._01_icd_ionescu import detected_ics, matches, metrics_all, prec_rec_f1_dict
 
     snapshot.assert_match(detected_ics, "short_trial_output")
+    snapshot.assert_match(matches.reset_index(), "matches", check_dtype=False)
+    snapshot.assert_match(pd.DataFrame(prec_rec_f1_dict, index=[0]), "prec_rec_f1_values")
+    snapshot.assert_match(pd.DataFrame(metrics_all, index=[0]), "metrics_all")
 
 
 def test_icd_shin_improved(snapshot):
