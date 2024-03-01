fa0b6da3edc5141b7839c7979d14a19336eac9c6
Arne Küderle <a.kuederle@gmail.com>
2024-03-01 16:54:20 +0100

    Adapted to mobilse aggregator to play nicely together with the behaivaur of the apply threshold func

--- examples/aggregation/_02_threshold_check.py
+++ examples/aggregation/_02_threshold_check.py
@@ -1,21 +1,53 @@
 """
+
+.. _threshold_check_example:
+
 Threshold Checker
 ====================
 
-This example shows how to use the functions threshold_check to check if the points fall within thresholds or not.
+This example shows how to use the functions ``apply_thresholds`` to check if a DMO falls within the thresholds for a
+specific cohort and height.
 """
 
 import pandas as pd
 
-from gaitlink.aggregation._threshold_check import apply_thresholds, get_mobilised_dmo_thresholds
+from gaitlink import PACKAGE_ROOT
+from gaitlink.aggregation import apply_thresholds, get_mobilised_dmo_thresholds
 
-# Absolute path to the input file
-input_data = pd.read_csv("gaitlink\\example_data\\original_results\\mobilised_aggregator\\aggregation_test_input.csv")
+# %%
+# Selecting Example Data
+# ----------------------
+# We use some example DMO data.
+# Note, that the ``apply_thresholds`` function can only be used with the data of a single participant at the time,
+# as meta-data such as the participant's height is required.
+# Luckily, the all the example data is from the same participant, just recorded at different times.
 
-# Extract Thresholds
-thresholds_data = get_mobilised_dmo_thresholds()
-output_data = apply_thresholds(input_data, thresholds_data, cohort="CHF", height_m=1.75)
-print(output_data)
+DATA_PATH = PACKAGE_ROOT.parent / "example_data/original_results/mobilised_aggregator"
 
-# Else save the file
-# output_data.to_csv(filename)
+data = pd.read_csv(DATA_PATH / "aggregation_test_input.csv", index_col=0).set_index(
+    ["visit_type", "participant_id", "measurement_date", "wb_id"]
+)
+data.head()
+
+# %%
+# Load Thresholds
+# ---------------
+thresholds = get_mobilised_dmo_thresholds()
+thresholds
+
+# %%
+# Apply Thresholds
+# ----------------
+output_data = apply_thresholds(data, thresholds, cohort="CHF", height_m=1.75)
+output_data.head()
+
+# %%
+# We can see that the output has exactly the same structure as the input data, but with boolean values indicating if
+# the DMO falls within the thresholds or not.
+# Columns for which no thresholds were provided are simply NaN, indicating that no filtering was applied.
+# It is up to you how you wnat to use this information.
+
+# %%
+# This output data can be used to filter the input data for DMOs that fall within the thresholds.
+# This can be done in combination with the aggregation algorithm to only include DMOs that fall within the thresholds.
+# See the :ref:`mobilised_aggregator example < s>` for more information.
