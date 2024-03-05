c00519a4715db4e50ed986fac65f675c0228d5d1
Annika Mücke <annika-muecke@outlook.de>
2024-03-05 13:50:37 +0100

    complete examples and their regression tests

--- tests/test_examples/test_icd.py
+++ tests/test_examples/test_icd.py
@@ -6,18 +6,19 @@ matplotlib.use("Agg")
 
 
 def test_icd_ionescu(snapshot):
-    from examples.icd._01_icd_ionescu import detected_ics, matches, metrics_all, prec_rec_f1_dict
+    from examples.icd._01_icd_ionescu import detected_ics, matches, metrics_all
 
     snapshot.assert_match(detected_ics, "short_trial_output")
-    snapshot.assert_match(matches.reset_index(), "matches", check_dtype=False)
-    snapshot.assert_match(pd.DataFrame(prec_rec_f1_dict, index=[0]), "prec_rec_f1_values")
+    snapshot.assert_match(matches, "matches")
     snapshot.assert_match(pd.DataFrame(metrics_all, index=[0]), "metrics_all")
 
 
 def test_icd_shin_improved(snapshot):
-    from examples.icd._02_shin_algo import detected_ics
+    from examples.icd._02_shin_algo import detected_ics, matches, metrics_all
 
     snapshot.assert_match(detected_ics, "short_trial_output")
+    snapshot.assert_match(matches, "matches")
+    snapshot.assert_match(pd.DataFrame(metrics_all, index=[0]), "metrics_all")
 
 
 def test_icd_hklee_improved(snapshot):
