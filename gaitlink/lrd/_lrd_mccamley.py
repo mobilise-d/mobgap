01b07c72e01714dbdc6819bb9f2141a34d239df8
Arne Küderle <a.kuederle@gmail.com>
2024-03-11 13:25:17 +0100

    Added better docstring and some slight improvments

--- gaitlink/lrd/_lrd_mccamley.py
+++ gaitlink/lrd/_lrd_mccamley.py
@@ -10,28 +10,59 @@ from gaitlink.data_transform.base import BaseFilter
 from gaitlink.lrd.base import BaseLRDetector, base_lrd_docfiller
 
 
+@base_lrd_docfiller
 class LrdMcCamley(BaseLRDetector):
-    """
-        This class uses the McCamley algorithm (later improved in Ullrich et al.) in order to predict whether each pre-determined initial contact (IC) corresponds to a left or a right step.
+    """McCamley algorithm for laterality detection of initial contacts.
 
-        In the original McCamley algorithm, the angular velocity around the vertical axis ("gyr_x") serves as the distinguishing factor for identifying left and right ICs. The process involves the following steps:
+    The McCamley algorithm uses the sign of the angular velocity either yaw or roll (or a combination of both) as the
+    distinguishing factor for identifying left and right ICs.
 
-            * Signal Pre-processing: Subtracting the signal mean and applying a low-pass filter (4th order Butterworth filter with a 2 Hz cut-off frequency).
-            * IC Assignment: Analyzing the sign of the filtered "gyr_x" value at the IC time point for classification. If the value is positive, the IC is attributed to the right foot; if negative, it's attributed to the left foot.
+    For this the respectice signal is filtered (high-pass to remove DC offset and low-pass to remove noise) and the sign
+    at the position of the IC is used to determine the laterality.
 
-        As a first extension to the original McCamley algorithm, the angular velocity around the anterior-posterior axis, "gyr_z", can resemble a periodic wave with a constant phase shift w.r.t. "gyr_x" after application of the low-pass filter described above. This is also a suitable input signal for the McCamley algorithm, when inverting the sign. A second and final extension to the original McCamley algorithm is to use the combination of the filtered signals for the vertical and anterior-posterior signals: gyr_comb = gyr_x - gyr_z.
-    :
-        The methodology used here is based on the following reference papers:
-        1) J. McCamley et al., “An enhanced estimate of initial contact and final contact instants of time using lower trunk inertial sensor data,” Gait & posture, 2012, available at:
+    The original algorithm uses the yaw signal, but Ullrich et al. [2] showed that a combination of yaw and roll signals
+    can improve the detection accuracy.
+    Further, instead of simple mean subtraction, Ullrich et al. [2] used a Butterworth bandpass filter to smooth the
+    signal.
+
+    Parameters
+    ----------
+    axis
+        The axis to use for the laterality detection. Can be one of "yaw", "roll", or "combined".
+        For "roll" and "combined" the sign of the "roll" signal is inverted, as it is phase shifted compared to the yaw
+        signal.
+    smoothing_filter
+        The filter to use for smoothing the signal. The filter should be a high-pass filter to remove the DC offset and
+        a low-pass filter to remove noise.
+        So any form of bandpass filter should be suitable.
+
+    Attributes
+    ----------
+    %(ic_lr_list_)s
+    smoothed_data_
+        The smoothed data used for the laterality detection.
+        This might be helpful for debugging or further analysis.
+
+    Other Parameters
+    ----------------
+    %(other_parameters)s
+
+
+    Notes
+    -----
+    .. [1] J. McCamley et al., “An enhanced estimate of initial contact and final contact instants of time using lower
+        trunk inertial sensor data,” Gait & posture, 2012, available at:
         https://www.sciencedirect.com/science/article/pii/S0966636212000707?via%3Dihub
 
-        2) Reference Papers: Ullrich M, Kuderle A, Reggi L, Cereatti A, Eskofier BM, Kluge F. Machine learning-based distinction of left and right foot contacts in lower back inertial sensor gait data. Annu Int Conf IEEE Eng Med Biol Soc. 2021, available at: https://ieeexplore.ieee.org/stamp/stamp.jsp?arnumber=9630653
+    .. [2] Ullrich M, Kuderle A, Reggi L, Cereatti A, Eskofier BM, Kluge F. Machine learning-based distinction of left
+        and right foot contacts in lower back inertial sensor gait data. Annu Int Conf IEEE Eng Med Biol Soc. 2021,
+        available at: https://ieeexplore.ieee.org/stamp/stamp.jsp?arnumber=9630653
     """
+
     axis: Literal["yaw", "roll", "combined"]
     smoothing_filter: BaseFilter
 
-    # TODO: Note that the smoothing filter must filter a DC offset
-    smoothed_data_: pd.DataFrame
+    smoothed_data_: pd.Series
 
     def __init__(
         self,
@@ -50,7 +81,7 @@ class LrdMcCamley(BaseLRDetector):
         ic_list: pd.DataFrame,
         *,
         sampling_rate_hz: float,
-        **_: Unpack[dict[str, Any]]
+        **_: Unpack[dict[str, Any]],
     ) -> Self:
         """%(detect_short)s.
 
@@ -65,7 +96,8 @@ class LrdMcCamley(BaseLRDetector):
         self.ic_list = ic_list
 
         # create a copy of ic_list, otherwise they will get modified when adding the predicted labels
-        ic_list = ic_list.copy()
+        # We also remove the "lr_label" column, if it exists, to avoid conflicts
+        ic_list = ic_list.copy().drop(columns="lr_label", errors="ignore")
 
         if self.axis == "yaw":
             selected_data = data["gyr_x"]
@@ -80,13 +112,15 @@ class LrdMcCamley(BaseLRDetector):
 
         # The use of the smoothing filter is an addition made by Ullrich et al. to the original McCamley algorithm.
         # Originally, simply the mean of the signal was subtracted from the signal
-        self.smoothed_data_ = self.smoothing_filter.clone().filter(
-            selected_data, sampling_rate_hz=self.sampling_rate_hz
-        ).filtered_data_
+        self.smoothed_data_ = (
+            self.smoothing_filter.clone()
+            .filter(selected_data, sampling_rate_hz=self.sampling_rate_hz)
+            .filtered_data_
+        )
 
         data_at_ic = self.smoothed_data_.iloc[ic_list["ic"].to_numpy()]
 
-        ic_list["lr"] = np.where(data_at_ic <= 0, "left", "right")
+        ic_list["lr_label"] = np.where(data_at_ic <= 0, "left", "right")
 
         self.ic_lr_list_ = ic_list
         return self
