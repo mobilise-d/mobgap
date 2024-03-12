a3e20b29e0ca1162b8df8c3aa4b4ffc8bd77a2e3
Arne Küderle <a.kuederle@gmail.com>
2024-03-12 11:06:01 +0100

    Removed pipeline stuff again for now

--- gaitlink/pipeline/base.py
+++
@@ -1,9 +0,0 @@
-from tpcp import Pipeline
-
-from gaitlink.data.base import BaseGaitlinkDataset
-
-
-class BaseMobilisedDataPipeline(Pipeline[BaseGaitlinkDataset]):
-    """Base class for pipelines that work with"""
-
-    def
