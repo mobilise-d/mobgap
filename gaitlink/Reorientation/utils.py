0b68fb649878ff43e9fdc93ba6f31e0b04b7027a
dmegaritis <dmgrei3@gmail.com>
2024-03-13 08:52:35 +0000

    First implementation of helper function to rotate acceleration according to quaternions

---
+++ gaitlink/Reorientation/utils.py
@@ -0,0 +1,85 @@
+"""Helper functions for reorientation of IMU data."""
+
+import numpy as np
+from scipy.spatial.transform import Rotation
+
+def acceleration(IMU: np.ndarray, q: np.ndarray) -> np.ndarray:
+    """Calculated the rotated acceleration based on the IMU data and the quaternions.
+
+    Parameters
+    ----------
+    IMU
+        The IMU data as an array of shape (n, 3).
+    q
+        The quaternions as an array of shape (n, 4).
+
+    Returns
+    -------
+    The rotated acceleration as an array of shape (n, 3).
+
+    """
+    a = np.zeros((len(q), 3))
+    for i in range(len(q)):
+        a[i, :] = quaterot(q[i, :], IMU[i, 0:3])
+    print(a)
+    return a
+
+def quaterot(q: np.ndarray, acc: np.ndarray, qacc=None) -> np.ndarray:
+    """Rotates the acceleration using the quaternion.
+
+    Parameters
+    ----------
+    q
+        The quaternion as an array of shape (4,).
+    acc
+        The acceleration as an array of shape (3,).
+
+    Returns
+    -------
+    The rotated acceleration as an array of shape (3,).
+
+    """
+    qacc = quatmultiply(q, quatmultiply(np.array([0, *acc]), conj(q)))
+    qacc = qacc[1:4].reshape(-1, 1).flatten()
+    return qacc
+
+def conj(q: np.ndarray) -> np.ndarray:
+    """Computes the conjugate of a quaternion. Python implementation of "quatconj" in MATLAB.
+
+    Parameters:
+    ----------
+    q (np.ndarray)
+        Quaternion as an array of shape (4,).
+
+    Returns
+        -------
+    np.ndarray: Conjugate of the input quaternion.
+
+    """
+
+    q[1:][q[1:] != 0] *= -1
+    return q
+
+def quatmultiply(r: np.ndarray, q: np.ndarray) -> np.ndarray:
+    """Computes the product of two quaternions. Python implementation of "quatmultiply" in MATLAB.
+
+    Parameters:
+    ----------
+    r (np.ndarray)
+        Quaternion as an array of shape (4,).
+
+    q (np.ndarray)
+        Quaternion as an array of shape (4,).
+
+    Returns
+    -------
+    np.ndarray: Product of the two input quaternions.
+
+    """
+    n0 = r[0] * q[0] - r[1] * q[1] - r[2] * q[2] - r[3] * q[3]
+    n1 = r[0] * q[1] + r[1] * q[0] + r[2] * q[3] - r[3] * q[2]
+    n2 = r[0] * q[2] - r[1] * q[3] + r[2] * q[0] + r[3] * q[1]
+    n3 = r[0] * q[3] + r[1] * q[2] - r[2] * q[1] + r[3] * q[0]
+
+    n = np.array([n0, n1, n2, n3])
+    return n
