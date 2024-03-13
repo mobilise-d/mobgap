a50058d6adf5c65a4558b70dddd6ac0731b76164
Annika Mücke <annika-muecke@outlook.de>
2024-03-13 16:41:37 +0100

    correct rebasing error and recreate snapshots because underlying data changed

--- tests/test_examples/test_gsd.py
+++ tests/test_examples/test_gsd.py
@@ -4,11 +4,11 @@ import pandas as pd
 def test_gsd_iluz(snapshot):
     from examples.gsd._01_gsd_iluz import (
         categorized_intervals,
-        general_metrics_dict,
+        matched_metrics_dict,
         long_trial_output,
         long_trial_output_modified,
         matches,
-        mobilised_metrics_dict,
+        unmatched_metrics_dict,
         short_trial_output,
     )
 
@@ -16,6 +16,6 @@ def test_gsd_iluz(snapshot):
     snapshot.assert_match(long_trial_output_modified.gs_list_, "long_trial_output_modified")
     snapshot.assert_match(short_trial_output.gs_list_, "short_trial_output")
     snapshot.assert_match(categorized_intervals, "categorized_intervals")
-    snapshot.assert_match(pd.DataFrame(general_metrics_dict, index=[0]), "general_metrics_dict")
-    snapshot.assert_match(pd.DataFrame(mobilised_metrics_dict, index=[0]), "mobilised_metrics_dict")
+    snapshot.assert_match(pd.DataFrame(matched_metrics_dict, index=[0]), "general_metrics_dict")
+    snapshot.assert_match(pd.DataFrame(unmatched_metrics_dict, index=[0]), "mobilised_metrics_dict")
     snapshot.assert_match(matches, "matches")
