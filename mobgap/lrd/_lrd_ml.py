21e3f8d80d9ada73b6d7e5559fd634b5215724ff
Alex Stihi <astihi1@sheffield.ac.uk>
2024-03-14 10:35:34 +0000

    fixed tests and cleaned up the code, following Arne's review

--- mobgap/lrd/_lrd_ml.py
+++ mobgap/lrd/_lrd_ml.py
@@ -75,19 +75,13 @@ class LrdUllrich(BaseLRDetector):
 
         @classmethod
         def _load_from_file(cls, model_name):
-            # get path to pretrained models and the name of all available model models
-            # base_dir = Path(os.getenv('MODEL_DIR', './pretrained_models'))
             base_dir = Path(__file__).parent / os.getenv('MODEL_DIR', 'pretrained_models')
-            valid_model_names = [f.name.rpartition('_')[0] for f in base_dir.glob('*') if f.is_file() and not f.name.startswith('.')]
-            # remove duplicates (as the same name exists for both the model and the scaler) and sort alphabetically
-            valid_model_names = list(set(valid_model_names))
-            valid_model_names.sort()
             
             if (model_name in cls._model_cache) and (model_name in cls._scaler_cache):
                 print("Loading cached model and scaler...")
                 return cls._model_cache[model_name], cls._scaler_cache[model_name]
 
-            if model_name in valid_model_names:
+            if model_name in ["msproject_all", "msproject_hc", "msproject_ms"]:
                 print(f"Loading {model_name} model and scaler from file...")
                 model_path = base_dir / f'{model_name}_model.gz'
                 model = joblib.load(model_path) 
@@ -103,53 +97,11 @@ class LrdUllrich(BaseLRDetector):
                 return model, scaler
             
             else:
-                raise NotImplementedError("The pretrained configuration specified is not supported")
-        
-        @classproperty    
-        def icicle_all(cls):
-            model, scaler = cls._load_from_file("icicle_all")
-            return {"smoothing_filter" : ButterworthFilter(order = 4, cutoff_freq_hz = (0.5, 2), filter_type = "bandpass"),
-                    "model": model, "scaler": scaler}
-
-        @classproperty
-        def icicle_all_all(cls):
-            model, scaler = cls._load_from_file("icicle_all_all")
-            return {"smoothing_filter" : ButterworthFilter(order = 4, cutoff_freq_hz = (0.5, 2), filter_type = "bandpass"),
-                    "model": model, "scaler": scaler}
-        
-        @classproperty    
-        def icicle_hc(cls):
-            model, scaler = cls._load_from_file("icicle_hc")
-            return {"smoothing_filter" : ButterworthFilter(order = 4, cutoff_freq_hz = (0.5, 2), filter_type = "bandpass"),
-                    "model": model, "scaler": scaler}
-        
-        @classproperty    
-        def icicle_hc_all(cls):
-            model, scaler = cls._load_from_file("icicle_hc_all")
-            return {"smoothing_filter" : ButterworthFilter(order = 4, cutoff_freq_hz = (0.5, 2), filter_type = "bandpass"),
-                    "model": model, "scaler": scaler}
-        
-        @classproperty    
-        def icicle_pd(cls):
-            model, scaler = cls._load_from_file("icicle_pd")
-            return {"smoothing_filter" : ButterworthFilter(order = 4, cutoff_freq_hz = (0.5, 2), filter_type = "bandpass"),
-                    "model": model, "scaler": scaler}
-        
-        @classproperty    
-        def icicle_pd_all(cls):
-            model, scaler = cls._load_from_file("icicle_pd_all")
-            return {"smoothing_filter" : ButterworthFilter(order = 4, cutoff_freq_hz = (0.5, 2), filter_type = "bandpass"),
-                    "model": model, "scaler": scaler}
+                raise NotImplementedError("The specified pretrained configuration is not supported")
         
         @classproperty    
         def msproject_all(cls):
             model, scaler = cls._load_from_file("msproject_all")
-            return {"smoothing_filter" : ButterworthFilter(order = 4, cutoff_freq_hz = (0.5, 2), filter_type = "bandpass"),
-                    "model": model, "scaler": scaler}
-        
-        @classproperty    
-        def msproject_all_all(cls):
-            model, scaler = cls._load_from_file("msproject_all_all")
             return {"smoothing_filter" : ButterworthFilter(order = 4, cutoff_freq_hz = (0.5, 2), filter_type = "bandpass"),
                     "model": model, "scaler": scaler}
         
@@ -160,92 +112,26 @@ class LrdUllrich(BaseLRDetector):
                     "model": model, "scaler": scaler}
         
         @classproperty    
-        def msproject_hc_all(cls):
-            model, scaler = cls._load_from_file("msproject_hc_all")
-            return {"smoothing_filter" : ButterworthFilter(order = 4, cutoff_freq_hz = (0.5, 2), filter_type = "bandpass"),
-                    "model": model, "scaler": scaler}
-        
-        @classproperty    
         def msproject_ms(cls):
             model, scaler = cls._load_from_file("msproject_ms")
-            return {"smoothing_filter" : ButterworthFilter(order = 4, cutoff_freq_hz = (0.5, 2), filter_type = "bandpass"),
-                    "model": model, "scaler": scaler}
-        
-        @classproperty    
-        def msproject_ms_all(cls):
-            model, scaler = cls._load_from_file("msproject_ms_all")
-            return {"smoothing_filter" : ButterworthFilter(order = 4, cutoff_freq_hz = (0.5, 2), filter_type = "bandpass"),
-                    "model": model, "scaler": scaler}
-        
-        @classproperty    
-        def uniss_unige_all(cls):
-            model, scaler = cls._load_from_file("uniss_unige_all")
-            return {"smoothing_filter" : ButterworthFilter(order = 4, cutoff_freq_hz = (0.5, 2), filter_type = "bandpass"),
-                    "model": model, "scaler": scaler}
-        
-        @classproperty    
-        def uniss_unige_all_all(cls):
-            model, scaler = cls._load_from_file("uniss_unige_all_all")
-            return {"smoothing_filter" : ButterworthFilter(order = 4, cutoff_freq_hz = (0.5, 2), filter_type = "bandpass"),
-                    "model": model, "scaler": scaler}
-        
-        @classproperty    
-        def uniss_unige_ch(cls):
-            model, scaler = cls._load_from_file("uniss_unige_ch")
-            return {"smoothing_filter" : ButterworthFilter(order = 4, cutoff_freq_hz = (0.5, 2), filter_type = "bandpass"),
-                    "model": model, "scaler": scaler}
-        
-        @classproperty    
-        def uniss_unige_ch_all(cls):
-            model, scaler = cls._load_from_file("uniss_unige_ch_all")
-            return {"smoothing_filter" : ButterworthFilter(order = 4, cutoff_freq_hz = (0.5, 2), filter_type = "bandpass"),
-                    "model": model, "scaler": scaler}
-        
-        @classproperty    
-        def uniss_unige_hc(cls):
-            model, scaler = cls._load_from_file("uniss_unige_hc")
-            return {"smoothing_filter" : ButterworthFilter(order = 4, cutoff_freq_hz = (0.5, 2), filter_type = "bandpass"),
-                    "model": model, "scaler": scaler}
-        
-        @classproperty    
-        def uniss_unige_hc_all(cls):
-            model, scaler = cls._load_from_file("uniss_unige_hc_all")
-            return {"smoothing_filter" : ButterworthFilter(order = 4, cutoff_freq_hz = (0.5, 2), filter_type = "bandpass"),
-                    "model": model, "scaler": scaler}
-        
-        @classproperty    
-        def uniss_unige_pd(cls):
-            model, scaler = cls._load_from_file("uniss_unige_pd")
-            return {"smoothing_filter" : ButterworthFilter(order = 4, cutoff_freq_hz = (0.5, 2), filter_type = "bandpass"),
-                    "model": model, "scaler": scaler}
-        
-        @classproperty    
-        def uniss_unige_pd_all(cls):
-            model, scaler = cls._load_from_file("uniss_unige_pd_all")
-            return {"smoothing_filter" : ButterworthFilter(order = 4, cutoff_freq_hz = (0.5, 2), filter_type = "bandpass"),
-                    "model": model, "scaler": scaler}
-        @classproperty    
-        def uniss_unige_st(cls):
-            model, scaler = cls._load_from_file("uniss_unige_st")
-            return {"smoothing_filter" : ButterworthFilter(order = 4, cutoff_freq_hz = (0.5, 2), filter_type = "bandpass"),
-                    "model": model, "scaler": scaler}
-        
-        @classproperty    
-        def uniss_unige_st_all(cls):
-            model, scaler = cls._load_from_file("uniss_unige_st_all")
             return {"smoothing_filter" : ButterworthFilter(order = 4, cutoff_freq_hz = (0.5, 2), filter_type = "bandpass"),
                     "model": model, "scaler": scaler}
 
 
     def __init__(self,
-                 model: ClassifierMixin = None,
-                 scaler: MinMaxScaler = None,
-                #  lower_band: float = 0.5,
-                #  upper_band: float = 2):
+                model: ClassifierMixin = None,
+                scaler: MinMaxScaler = None,
                 smoothing_filter: BaseFilter = cf(ButterworthFilter(order = 4, cutoff_freq_hz = (0.5, 2), filter_type="bandpass"))
     ) -> None:
-        self.model = model
-        self.scaler = scaler
+        super().__init__()
+        if model is None:
+            self.model = self.PredefinedParameters.msproject_all['model']
+        else:
+            self.model = model
+        if scaler is None:
+            self.scaler = self.PredefinedParameters.msproject_all['scaler']
+        else:
+            self.scaler = scaler
         self.smoothing_filter = smoothing_filter
 
     # Model checking
@@ -264,7 +150,6 @@ class LrdUllrich(BaseLRDetector):
         model : ClassifierMixin
             The checked model.
         """
-        # print("Model checking...")
         if isinstance(model, ClassifierMixin):
             return model
         raise TypeError(f"Unknown model type {type(model)}. The model must be of type {ClassifierMixin}")
@@ -292,17 +177,19 @@ class LrdUllrich(BaseLRDetector):
             
         if not isinstance(ic_list, pd.DataFrame):
             raise TypeError("'ic_list' must be a pandas DataFrame")
-        
-        # Check if data and ic_list are empty
-        if data.empty:
-            raise ValueError("'data' must contain at least one data point")
-        if ic_list.empty:
-            raise ValueError("'ic_list' must contain at least one initial contact point")
-
 
         self.data = data
         self.ic_list = ic_list
         self.sampling_rate_hz = sampling_rate_hz
+
+        if data.empty:
+            self.ic_lr_list_ = pd.DataFrame(columns = ["ic", "lr_label"])
+            self.feature_matrix_ = pd.DataFrame(columns = ["filtered_gyr_x", "gradient_gyr_x", "diff_2_gyr_x", "filtered_gyr_z", "gradient_gyr_z", "diff_2_gyr_z"])
+            return self
+        if ic_list.empty:
+            self.ic_lr_list_ = pd.DataFrame(columns = ["ic", "lr_label"])
+            self.feature_matrix_ = pd.DataFrame(columns = ["filtered_gyr_x", "gradient_gyr_x", "diff_2_gyr_x", "filtered_gyr_z", "gradient_gyr_z", "diff_2_gyr_z"])
+            return self
 
         self.model = self._check_and_init_model(self.model)
 
@@ -318,14 +205,12 @@ class LrdUllrich(BaseLRDetector):
         # Extract the features and scale the feature matrix to ensure uniformity.
         feature_matrix = self.extract_features(self.data, ic_list, self.sampling_rate_hz)
         self.feature_matrix_ = pd.DataFrame(self.scaler.transform(feature_matrix.values), columns = feature_matrix.columns, index = feature_matrix.index)
-
         prediction_per_gs = pd.DataFrame(self.model.predict(self.feature_matrix_.to_numpy()), columns = ["lr_label"])
         
         # Map model's output 0s and 1s to 'left' and 'right'
         mapping = {0: "left", 1: "right"}
         prediction_per_gs = prediction_per_gs.replace(mapping)
 
-        # Flatten ic_list and create DataFrame
         self.ic_lr_list_ = pd.DataFrame({"ic": self.ic_list.values.flatten(), "lr_label": prediction_per_gs["lr_label"]})
 
         return self
@@ -362,26 +247,20 @@ class LrdUllrich(BaseLRDetector):
         if not isinstance(ic_list, list):
             raise TypeError("'ic_list' must be a list of pandas DataFrames")
         
-        # preprocess data for all GSs.
         features = [self.extract_features(data, ic, sampling_rate_hz) for data, ic in zip(data_list, ic_list)]
 
         # Concatenate the features if there is more than one GS
         all_features = pd.concat(features, axis=0, ignore_index=True) if len(features) > 1 else features[0]
 
-        # Initialize the scaler if it's not already set
-        if self.scaler is None:
-            self.scaler = MinMaxScaler()
-
-        # Fit the scaler if it hasn't been fitted yet, otherwise just transform the data
-        if not hasattr(self.scaler, 'scale_'):
-            all_features = pd.DataFrame(self.scaler.fit_transform(all_features.values), columns=all_features.columns, index=all_features.index)
-        else:
+        try:
+            check_is_fitted(self.scaler)
             all_features = pd.DataFrame(self.scaler.transform(all_features.values), columns=all_features.columns, index=all_features.index)
+        except NotFittedError:
+            all_features = pd.DataFrame(self.scaler.fit_transform(all_features.values), columns=all_features.columns, index=all_features.index)
 
         # Concatenate the labels if there is more than one GS
         all_labels = pd.concat(label_list, axis=0, ignore_index=True) if len(label_list) > 1 else label_list[0]
 
-        # Fit the model with the features and labels
         self.model.fit(all_features.to_numpy(), np.ravel(all_labels))
 
         return self
@@ -403,26 +282,27 @@ class LrdUllrich(BaseLRDetector):
         ics (pd.DataFrame): The initial contact list.
         sampling_rate_hz (float): The sampling rate in Hz.
 
+        Note:
+        ---------------
+        The last initial contact (IC) is shifted by 3 samples to ensure that the second derivative can be calculated. This shift only occurs if the last IC is within 3 samples of the end of the data. Additionally, any ICs that are less than 2 are set to 2 to avoid extracting rows with NaN values due to the calculation of the derivatives.
+
         Returns:
         ---------------
         feature_df (pd.DataFrame): The DataFrame containing the extracted features.
         """
-        # copy ics, otherwise it will be modified by ics -= 1 and then shifted.
         ics = ics.copy()
-        
-        # Apply Butterworth filtering and extract the first and second derivatives.        
-        gyr = data[["gyr_x", "gyr_z"]].rename({"gyr_x": "v", "gyr_z": "ap"})
+        gyr = data[["gyr_x", "gyr_z"]]
         gyr_filtered = self.smoothing_filter.clone().filter(gyr, sampling_rate_hz = sampling_rate_hz).filtered_data_
         gyr_diff = gyr_filtered.diff()
         gyr_diff_2 = gyr_diff.diff()
 
-        # Combine filtered data and its derivatives into a single DataFrame
         signal_paras = pd.concat({"filtered": gyr_filtered, "gradient": gyr_diff, "diff_2": gyr_diff_2}, axis=1)
         # Squash the multi index
         signal_paras.columns = ["_".join(c) for c in signal_paras.columns]
 
-        # shift the last IC by 3 samples to make the second derivative work    
-        ics -= 1
+        # shift the last IC by 3 samples to make the second derivative work
+        if (ics.iloc[-1]).any() >= len(signal_paras):
+            ics.iloc[-1] -= 3
         ics[ics < 2] = 2
 
         # Extract features corresponding to the adjusted ics values
