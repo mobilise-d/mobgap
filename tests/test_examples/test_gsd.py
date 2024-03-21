16d08e32d220ed9913f6a37f9ba90ec5f2102a04
Annika Mücke <annika-muecke@outlook.de>
2024-03-21 13:03:50 +0100

    add regression tests for example

--- tests/test_examples/test_gsd.py
+++ tests/test_examples/test_gsd.py
@@ -3,18 +3,28 @@ import pandas as pd
 
 def test_gsd_iluz(snapshot):
     from examples.gsd._01_gsd_iluz import (
-        categorized_intervals,
         long_trial_output,
         long_trial_output_modified,
-        matched_metrics_dict,
-        matches,
         short_trial_output,
-        unmatched_metrics_dict,
     )
 
     snapshot.assert_match(long_trial_output.gs_list_, "long_trial_output")
     snapshot.assert_match(long_trial_output_modified.gs_list_, "long_trial_output_modified")
     snapshot.assert_match(short_trial_output.gs_list_, "short_trial_output")
+
+
+def test_gsd_evaluation(snapshot):
+    from examples.gsd._02_gsd_evaluation import (
+        categorized_intervals,
+        detected_gsd_list,
+        matched_metrics_dict,
+        matches,
+        reference_gsd_list,
+        unmatched_metrics_dict,
+    )
+
+    snapshot.assert_match(reference_gsd_list, "reference_gsd_list")
+    snapshot.assert_match(detected_gsd_list, "detected_gsd_list")
     snapshot.assert_match(categorized_intervals, "categorized_intervals")
     snapshot.assert_match(pd.DataFrame(matched_metrics_dict, index=[0]), "general_metrics_dict")
     snapshot.assert_match(pd.DataFrame(unmatched_metrics_dict, index=[0]), "mobilised_metrics_dict")
