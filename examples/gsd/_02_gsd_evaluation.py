1dcec0c7b53f83f86cbf321aa3133463dc8fcb5c
Arne Küderle <a.kuederle@gmail.com>
2024-03-25 13:49:25 +0100

    Some rewording of example and removed some redundant snapshots

--- examples/gsd/_02_gsd_evaluation.py
+++ examples/gsd/_02_gsd_evaluation.py
@@ -57,22 +57,31 @@ reference_gsd_list
 # --------------------------------------------------
 # Let's quantify how the algorithm output compares to the reference labels.
 # To gain a detailed insight into the performance of the algorithm, we can look into the individual matches between the
-# detected and reference initial contacts.
+# detected and reference gait sequences.
+# Note, that there are two different ways to approach this:
+#
+# 1. We can calculate the for each sample, whether it is correctly detected as gait or not.
+# 2. We can check on the level of gait sequences, whether a detected gait sequence matches with a reference gait sequence (by a certain overlap threshold).
+#
+# In mobgap, we provide functions to calculate both types of performance metrics. Let's start with the first one.
+#
+# Sample-wise performance evaluation
+# ----------------------------------
 # To do this, we use the :func:`~gaitlink.gsd.evaluation.categorize_intervals` function
 # to identify overlapping regions between the detected gait sequences and the reference gait sequences.
+# These overlapping regions can then be converted into sample-wise classifications of true positives, false positives, and false negatives.
+#
 # As function arguments, besides the mandatory detected and reference gait sequences,
 # the total number of samples in the recording can be specified as optional parameter.
-# If provided, the intervals where no gait sequences are present in the reference and the detected list are
-# also reported.
-# Later on, we can then use these categorized intervals to calculate a range of higher-level performance metrics.
+# If provided, the intervals where no gait sequences are present in the reference and the detected list are also reported.
+# Later on, we can then use these categorized intervals to calculate a set of higher-level performance metrics.
 #
 # As result, a DataFrame containing `start` and `end`  index of the resulting categorized intervals together with
 # a `match_type` column that contains the type of match for each interval, i.e. `tp` for true positive, `fp` for false
 # positive, and `fn` for false negative.
 # These intervals can not be interpreted as gait sequences, but are rather subsequences of the detected gait sequences
 # categorizing correctly detected samples (`tp`), falsely detected samples (`fp`), samples from the reference gsd
-# list that were not detected (`fn`), and (optionally) samples where no gait sequences are present in both the reference
-# and detected gait sequences (`tn`).
+# list that were not detected (`fn`), and (optionally) samples where no gait sequences are present in both the reference and detected gait sequences (`tn`).
 # Note that the tn intervals are not explicitly calculated, but are inferred from the total length of the recording
 # (if provided) and from the other intervals, as everything between them is considered as true negative.
 from mobgap.gsd.evaluation import categorize_intervals
@@ -89,7 +98,7 @@ categorized_intervals
 # Based on the individually categorized tp, fp, fn, and tn intervals, common performance metrics,
 # e.g., F1 score, precision, or recall can be calculated.
 # For this purpose, the :func:`~gaitlink.gsd.evaluation.calculate_matched_gsd_performance_metrics` function can be used.
-# It calculates the metrics based on the 'matched' gsd intervals, i.e., the categorized interval list where every entry
+# It calculates the metrics based on the "matched" gsd intervals, i.e., the categorized interval list where every entry
 # has a match type (tp, fp, fn, tn) assigned.
 # Therefore, the function requires to call the :func:`~gaitlink.gsd.evaluation.categorize_intervals` function first.
 # The categorized intervals can then be passed as an argument
@@ -105,15 +114,13 @@ matched_metrics_dict = calculate_matched_gsd_performance_metrics(categorized_int
 matched_metrics_dict
 
 # %%
-# Furthermore, there is a range of high-level performance metrics that is calculated based
-# on the overall amount of gait sequences in reference and detected data.
+# Furthermore, there is a range of high-level performance metrics that are simply calculated based
+# on the overall amount of gait sequences/gait detected in reference and detected data.
 # Thus, they can be inferred from the reference and detected gait sequences directly without any intermediate steps
-# using the :func:`~gaitlink.gsd.evaluation.calculate_unmatched_gsd_performance_metrics`
-# function.
+# using the :func:`~gaitlink.gsd.evaluation.calculate_unmatched_gsd_performance_metrics` function.
 # As some of the unmatched metrics are reported in seconds, the function requires the sampling frequency of the recorded
 # data as an additional argument.
-# It requires specifying the sampling frequency of the recorded data (to calculate the duration errors in seconds)
-# and returns a dictionary containing all metrics for the specified detected and reference gait sequences.
+# It returns a dictionary containing all metrics for the specified detected and reference gait sequences.
 from mobgap.gsd.evaluation import calculate_unmatched_gsd_performance_metrics
 
 unmatched_metrics_dict = calculate_unmatched_gsd_performance_metrics(
@@ -124,17 +131,27 @@ unmatched_metrics_dict = calculate_unmatched_gsd_performance_metrics(
 
 unmatched_metrics_dict
 # %%
+# Direct Gaits Sequence Matching
+# ------------------------------
 # Apart from the performance evaluation methods mentioned above, it might be useful in some cases to identify
 # how many and which detected gait sequences reliably match with the ground truth.
-# This can be helpful for instance when developing a new detection algorithm or when investigating aggregated parameters
-# from within the respective gait sequences.
+#
+# This is primarily useful, when further parameters are associated with each gaits sequence, e.g., the gait speed.
+# In this case, matching gait sequences that cover the same gait regions allows proper comparison of these parameters.
+# For more information on this, see the example on the overall parameter evaluation on Walking-Bout level (TODO).
+#
 # For this purpose, the :func:`~gaitlink.gsd.evaluation.find_matches_with_min_overlap` can be used.
 # It returns all intervals of the detected gait sequences that overlap with the reference gait sequences by at least a
 # given amount.
 # The index of the result dataframe indicated the index of the detected gait sequence.
 # We can see that with an overlap threshold of 0.7 (70%), three of the six detected gait sequences are considered as
 # matches with the reference gait sequences for our example recording.
-# The remaining ones either contain too many false positive and/or false negative samples.
+# Note, that this threshold is enforeced in both directions, i.e., the detected gait sequence must overlap with the
+# reference gait sequence by at least 70% and vice versa.
+# This means that only 1 to 1 matches are possible.
+# If multiple detected gait sequences overlap with the same reference gait sequence, only the one with the highest overlap
+# is considered as a match.
+# If one gait sequence is covered by multiple smaller once, possibly none of them is considered as a match.
 from mobgap.gsd.evaluation import find_matches_with_min_overlap
 
 matches = find_matches_with_min_overlap(
