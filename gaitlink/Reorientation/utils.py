7cb05d45874b447a9f3b7639ed975b17e398d0f6
dmegaritis <dmgrei3@gmail.com>
2024-03-28 11:29:34 +0000

    Updated quat multiplication in line with MATLAB

--- gaitlink/Reorientation/utils.py
+++ gaitlink/Reorientation/utils.py
@@ -76,9 +76,9 @@ def quatmultiply(r: np.ndarray, q: np.ndarray) -> np.ndarray:
 
     """
     n0 = r[0] * q[0] - r[1] * q[1] - r[2] * q[2] - r[3] * q[3]
-    n1 = r[0] * q[1] + r[1] * q[0] - r[2] * q[3] + r[3] * q[2]
-    n2 = r[0] * q[2] + r[1] * q[3] + r[2] * q[0] - r[3] * q[1]
-    n3 = r[0] * q[3] - r[1] * q[2] + r[2] * q[1] + r[3] * q[0]
+    n1 = r[0] * q[1] + r[1] * q[0] + r[2] * q[3] - r[3] * q[2]
+    n2 = r[0] * q[2] - r[1] * q[3] + r[2] * q[0] + r[3] * q[1]
+    n3 = r[0] * q[3] + r[1] * q[2] - r[2] * q[1] + r[3] * q[0]
 
     n = np.array([n0, n1, n2, n3])
     return n
