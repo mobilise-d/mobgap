502504856c73a1c2e196875ca73aadbe4fb8c885
dmegaritis <dmgrei3@gmail.com>
2024-07-04 15:38:18 +0100

    Use of iter_gs

--- mobgap/Reorientation/CorrectOrientationSensorAxes.py
+++ mobgap/Reorientation/CorrectOrientationSensorAxes.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import scipy.constants
+from mobgap.pipeline import iter_gs
 from mobgap.Reorientation.CorrectSensorOrientationDynamic import CorrectSensorOrientationDynamic
 from mobgap.gsd import GsdIluz
 from mobgap.Reorientation.filteringsignals_100Hz import filtering_signals_100hz
@@ -69,50 +70,59 @@ def CorrectOrientationSensorAxes(data: pd.DataFrame, sampling_rate_hz: float) ->
 
         av_filt1 = chain_transformers(av_filt, filter_chain, sampling_rate_hz=sampling_rate_hz)
 
+        # Output of filter chains should be a pd.DataFrame so the iter_gs function can work
+        av_filt1 = pd.DataFrame(av_filt1, columns=['acc_x'])
+
         gs = GsdIluz().detect(acc, sampling_rate_hz=sampling_rate_hz).gs_list_
 
-        gsLabel = np.zeros(len(acc.iloc[:, 0]))
+        # Adding a specific index to gs so the iter_gs function can work
+        if gs.index.name is None:
+            gs['gs_id'] = range(len(gs))
+        else:
+            if gs.index.name != 'gs_id' and gs.index.name != 'wb_id':
+                gs['gs_id'] = range(len(gs))
+
+        gsLabel = np.zeros(len(acc.loc[:, "acc_x"]))
         n = max(gs.shape)
         k = 0
 
-        GS = pd.DataFrame(columns=['Start', 'Stop'])
-
         if n > 2:
-            for i in gs.index:
-                GS.loc[i, 'Start'] = gs.loc[i, 'start']
-                GS.loc[i, 'Stop'] = gs.loc[i, 'end']
+            for GS, data_slice in iter_gs(av_filt1, gs):
 
-                # start and end are already in samples so no need to multiply with fs as done in MATLAB
-                l1 = gs.loc[i, 'start']
-                l2 = gs.loc[i, 'end']
+                gsLabel[GS.start:GS.end] = 1
 
-                gsLabel[l1:l2] = 1
-
-                avm = np.mean(av_filt1[l1:l2])
+                avm = np.mean(data_slice)
 
                 if avm >= th:
-                    corIMUdata.iloc[l1:l2, 0:3] = acc.iloc[l1:l2, :]
+                    corIMUdata.iloc[GS.start:GS.end, 0:3] = acc.iloc[GS.start:GS.end, :]
                 elif avm <= -th:
-                    corIMUdata.iloc[l1:l2, 0] = -acc.iloc[l1:l2, 0]
-                    corIMUdataSequence.loc[k, ['Start', 'End']] = [gs.loc[i, 'start'], gs.loc[i, 'end']]
+                    corIMUdata.iloc[GS.start:GS.end, 0] = -acc.iloc[GS.start:GS.end, 0]
+                    corIMUdataSequence.loc[k, ['Start', 'End']] = [GS.start, GS.end]
                     k = k + 1
                 else:
-                    i = int(i)
-                    corIMUdata.iloc[l1:l2, :] = CorrectSensorOrientationDynamic(data.iloc[l1:l2, :], sampling_rate_hz)
-                    corIMUdataSequence.loc[k, ['Start', 'End']] = [gs.loc[i, 'start'], gs.loc[i, 'end']]
+                    corIMUdata.iloc[GS.start:GS.end, :] = CorrectSensorOrientationDynamic(data.iloc[GS.start:GS.end, :], sampling_rate_hz)
+                    corIMUdataSequence.loc[k, ['Start', 'End']] = [GS.start, GS.end]
                     k = k + 1
 
         ind_noGS = groupfind(gsLabel == 0)
 
-        for i in range(len(ind_noGS[:, 0])):
-            l1 = ind_noGS[i, 0]
-            l2 = ind_noGS[i, 1]
-            avm = np.mean(av_filt1[l1:l2])
+        # ind_noGS should be a pd.DataFrame so the iter_gs function can work
+        ind_noGS = pd.DataFrame(ind_noGS, columns=['start', 'end'])
+
+        # Adding a specific index to ind_noGS so the iter_gs function can work
+        if ind_noGS.index.name is None:
+            ind_noGS['gs_id'] = range(len(ind_noGS))
+        else:
+            if ind_noGS.index.name != 'gs_id' and ind_noGS.index.name != 'wb_id':
+                ind_noGS['gs_id'] = range(len(ind_noGS))
+
+        for GS, data_slice in iter_gs(av_filt1, ind_noGS):
+            avm = np.mean(data_slice)
 
             if avm >= th:
-                corIMUdata.iloc[l1:l2, 0:3] = acc.iloc[l1:l2, :]
+                corIMUdata.iloc[GS.start:GS.end, 0:3] = acc.iloc[GS.start:GS.end, :]
             elif avm <= -th:
-                corIMUdata.iloc[l1:l2, 0] = -acc.iloc[l1:l2, 0]
+                corIMUdata.iloc[GS.start:GS.end, 0] = -acc.iloc[GS.start:GS.end, 0]
 
     else:
         return corIMUdata, corIMUdataSequence
