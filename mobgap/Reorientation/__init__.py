4611c8d9763c63dc419708409aef0b3cc56eb1fa
dmegaritis <dmgrei3@gmail.com>
2024-07-09 10:51:43 +0100

    Updated init

--- mobgap/Reorientation/__init__.py
+++ mobgap/Reorientation/__init__.py
@@ -1,5 +1,5 @@
 """Algorithms to rotate the signal according to the position of the IMU."""
 
-from mobgap.Reorientation import CorrectOrientationSensorAxes, CorrectSensorOrientationDynamic, filteringsignals_100Hz
+from mobgap.Reorientation import CorrectOrientationSensorAxes, CorrectSensorOrientationDynamic, _filteringsignals_100Hz
 
-__all__ = ["CorrectSensorOrientationDynamic", "CorrectOrientationSensorAxes", "filteringsignals_100Hz"]
+__all__ = ["CorrectSensorOrientationDynamic", "CorrectOrientationSensorAxes", "_filteringsignals_100Hz"]
