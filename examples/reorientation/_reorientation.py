089d1023ea71d662eae5714e1238df1952388ed1
dmegaritis <dmgrei3@gmail.com>
2024-09-24 16:56:01 +0100

    Implemented examples with disoriented data

--- examples/reorientation/_reorientation.py
+++ examples/reorientation/_reorientation.py
@@ -2,7 +2,7 @@
 Reorientation
 =========
 
-This example shows how to use the amended reorietation block and some examples on how the signal is corrected.
+This example shows how to use the amended reorientation block and some examples on how the signal is corrected.
 
 """
 
@@ -19,12 +19,11 @@ import matplotlib.pyplot as plt
 # We load the example data from the lab dataset together with 8 different datasets with wrong orientation
 # based on the same example data.
 from mobgap.data import LabExampleDataset
+from mobgap.utils.conversions import to_body_frame
 
 example_data = LabExampleDataset(reference_system="INDIP", reference_para_level="wb")
 single_test = example_data.get_subset(cohort="HA", participant_id="001", test="Test11", trial="Trial1")
-imu_data = single_test.data_ss
-IMU_correct = imu_data
-
+IMU = to_body_frame(single_test.data_ss)
 
 # %%
 # Applying the algorithm example
@@ -33,16 +32,12 @@ IMU_correct = imu_data
 from mobgap.Reorientation._correct_orientation_sensor_axes import CorrectOrientationSensorAxes
 
 reorientation = CorrectOrientationSensorAxes(sampling_rate_hz=100)
-corrected = reorientation.update_orientation(IMU_correct)
+corrected = reorientation.update_orientation(IMU)
 
 corrected_IMU = corrected.corIMUdata
 corrected_IMU
 corrected_IMU_sequence = corrected.corIMUdataSequence
 corrected_IMU_sequence
-
-
-# TODO: load 8 wrong files, names could be IMU_*** based on the wrong orientation name
-
 
 # %%
 
@@ -52,9 +47,9 @@ corrected_IMU_sequence
 fig, (pl1, pl2) = plt.subplots(2, 1, figsize=(10, 12))
 
 # Pre reorientation
-pl1.plot(corrected_IMU['index'], corrected_IMU['acc_is'], label='inferior-superior', color='green')
-pl1.plot(corrected_IMU['index'], corrected_IMU['acc_ml'], label='posterior-anterior', color='red')
-pl1.plot(corrected_IMU['index'], corrected_IMU['acc_pa'], label='medial-lateral', color='blue')
+pl1.plot(IMU.index, IMU['acc_is'], label='inferior-superior', color='green')
+pl1.plot(IMU.index, IMU['acc_ml'], label='posterior-anterior', color='red')
+pl1.plot(IMU.index, IMU['acc_pa'], label='medial-lateral', color='blue')
 pl1.set_xlabel('Index')
 pl1.set_ylabel('Acc')
 pl1.set_title('Pre')
@@ -78,21 +73,27 @@ plt.show()
 # ----------------------
 # Below we apply the reorientation block to a lab trial with wrongly oriented data.
 # The signal is rotated by 30 degrees on the ML axis to the opposite direction than the convention.
-# TODO: add "Body Curvature"
 
+# Loading the data
+path = 'example_data/data_csv/data_reorientation/BodyCurvature.csv'
+IMU_BD = pd.read_csv(path)
+
+reorientation = CorrectOrientationSensorAxes(sampling_rate_hz=100)
+corrected = reorientation.update_orientation(IMU_BD)
+
+corrected_IMU = corrected.corIMUdata
+corrected_IMU
+corrected_IMU_sequence = corrected.corIMUdataSequence
+corrected_IMU_sequence
 
 # Visualizing the signal before and after reorientation
 # Below we visualize the signal before and after reorientation in the above file
-# TODO: ammend df names to reflect the pre and post reorientation
-
-corrected_IMU = IMU_correct
-
 fig, (pl1, pl2) = plt.subplots(2, 1, figsize=(10, 12))
 
 # Pre reorientation
-pl1.plot(corrected_IMU['index'], corrected_IMU['acc_is'], label='inferior-superior', color='green')
-pl1.plot(corrected_IMU['index'], corrected_IMU['acc_ml'], label='posterior-anterior', color='red')
-pl1.plot(corrected_IMU['index'], corrected_IMU['acc_pa'], label='medial-lateral', color='blue')
+pl1.plot(IMU_BD.index, IMU_BD['acc_is'], label='inferior-superior', color='green')
+pl1.plot(IMU_BD.index, IMU_BD['acc_ml'], label='posterior-anterior', color='red')
+pl1.plot(IMU_BD.index, IMU_BD['acc_pa'], label='medial-lateral', color='blue')
 pl1.set_xlabel('Index')
 pl1.set_ylabel('Acc')
 pl1.set_title('Pre')
@@ -115,67 +116,284 @@ plt.show()
 # ----------------------
 # Below we apply the reorientation block to a lab trial with wrongly oriented data.
 # The signal comes from a sensor placed upside downvertical and mediolateral axes will be inverted
-# TODO: add "Upsidedown"
+
+# Loading the data for Upsidedown
+path = 'example_data/data_csv/data_reorientation/Upsidedown.csv'  # Adjust the file path
+IMU_Upsidedown = pd.read_csv(path)
+
+# Assuming CorrectOrientationSensorAxes is defined elsewhere in your code
+reorientation = CorrectOrientationSensorAxes(sampling_rate_hz=100)
+corrected = reorientation.update_orientation(IMU_Upsidedown)
+
+corrected_IMU = corrected.corIMUdata
+corrected_IMU_sequence = corrected.corIMUdataSequence
 
 # Visualizing the signal before and after reorientation
-# Below we visualize the signal before and after reorientation in the above file
+fig, (pl1, pl2) = plt.subplots(2, 1, figsize=(10, 12))
+
+# Pre reorientation
+pl1.plot(IMU_Upsidedown.index, IMU_Upsidedown['acc_is'], label='inferior-superior', color='green')
+pl1.plot(IMU_Upsidedown.index, IMU_Upsidedown['acc_ml'], label='posterior-anterior', color='red')
+pl1.plot(IMU_Upsidedown.index, IMU_Upsidedown['acc_pa'], label='medial-lateral', color='blue')
+pl1.set_xlabel('Index')
+pl1.set_ylabel('Acc')
+pl1.set_title('Pre')
+pl1.legend()
+
+# Post reorientation
+pl2.plot(corrected_IMU['index'], corrected_IMU['acc_is'], label='inferior-superior', color='green')
+pl2.plot(corrected_IMU['index'], corrected_IMU['acc_ml'], label='posterior-anterior', color='red')
+pl2.plot(corrected_IMU['index'], corrected_IMU['acc_pa'], label='medial-lateral', color='blue')
+pl2.set_xlabel('Index')
+pl2.set_ylabel('Acc')
+pl2.set_title('Post')
+pl2.legend()
+
+plt.tight_layout()
+plt.show()
 
 # %%
 # Applying the algorithm in wrongly oriented data
 # ----------------------
 # Below we apply the reorientation block to a lab trial with wrongly oriented data.
 # The signal comes from a sensor placed inside out, mediolateral and anteroposterior are inverted.
-# TODO: add "InsideOut"
+
+# Loading the data for InsideOut
+path = 'example_data/data_csv/data_reorientation/InsideOut.csv'  # Adjust the file path
+IMU_InsideOut = pd.read_csv(path)
+
+# Assuming CorrectOrientationSensorAxes is defined elsewhere in your code
+reorientation = CorrectOrientationSensorAxes(sampling_rate_hz=100)
+corrected = reorientation.update_orientation(IMU_InsideOut)
+
+corrected_IMU = corrected.corIMUdata
+corrected_IMU_sequence = corrected.corIMUdataSequence
 
 # Visualizing the signal before and after reorientation
-# Below we visualize the signal before and after reorientation in the above file
+fig, (pl1, pl2) = plt.subplots(2, 1, figsize=(10, 12))
+
+# Pre reorientation
+pl1.plot(IMU_InsideOut.index, IMU_InsideOut['acc_is'], label='inferior-superior', color='green')
+pl1.plot(IMU_InsideOut.index, IMU_InsideOut['acc_ml'], label='posterior-anterior', color='red')
+pl1.plot(IMU_InsideOut.index, IMU_InsideOut['acc_pa'], label='medial-lateral', color='blue')
+pl1.set_xlabel('Index')
+pl1.set_ylabel('Acc')
+pl1.set_title('Pre')
+pl1.legend()
+
+# Post reorientation
+pl2.plot(corrected_IMU['index'], corrected_IMU['acc_is'], label='inferior-superior', color='green')
+pl2.plot(corrected_IMU['index'], corrected_IMU['acc_ml'], label='posterior-anterior', color='red')
+pl2.plot(corrected_IMU['index'], corrected_IMU['acc_pa'], label='medial-lateral', color='blue')
+pl2.set_xlabel('Index')
+pl2.set_ylabel('Acc')
+pl2.set_title('Post')
+pl2.legend()
+
+plt.tight_layout()
+plt.show()
 
 # %%
 # Applying the algorithm in wrongly oriented data
 # ----------------------
 # Below we apply the reorientation block to a lab trial with wrongly oriented data.
 # The signal comes from a sensor placed upside down and inside out, vertical and anteroposterior are inverted
-# TODO: add "UpsidedownInsideOut"
+
+# Loading the data for UpsidedownInsideOut
+path = 'example_data/data_csv/data_reorientation/UpsidedownInsideOut.csv'  # Adjust the file path
+IMU_UpsidedownInsideOut = pd.read_csv(path)
+
+# Assuming CorrectOrientationSensorAxes is defined elsewhere in your code
+reorientation = CorrectOrientationSensorAxes(sampling_rate_hz=100)
+corrected = reorientation.update_orientation(IMU_UpsidedownInsideOut)
+
+corrected_IMU = corrected.corIMUdata
+corrected_IMU_sequence = corrected.corIMUdataSequence
 
 # Visualizing the signal before and after reorientation
-# Below we visualize the signal before and after reorientation in the above file
+fig, (pl1, pl2) = plt.subplots(2, 1, figsize=(10, 12))
+
+# Pre reorientation
+pl1.plot(IMU_UpsidedownInsideOut.index, IMU_UpsidedownInsideOut['acc_is'], label='inferior-superior', color='green')
+pl1.plot(IMU_UpsidedownInsideOut.index, IMU_UpsidedownInsideOut['acc_ml'], label='posterior-anterior', color='red')
+pl1.plot(IMU_UpsidedownInsideOut.index, IMU_UpsidedownInsideOut['acc_pa'], label='medial-lateral', color='blue')
+pl1.set_xlabel('Index')
+pl1.set_ylabel('Acc')
+pl1.set_title('Pre')
+pl1.legend()
+
+# Post reorientation
+pl2.plot(corrected_IMU['index'], corrected_IMU['acc_is'], label='inferior-superior', color='green')
+pl2.plot(corrected_IMU['index'], corrected_IMU['acc_ml'], label='posterior-anterior', color='red')
+pl2.plot(corrected_IMU['index'], corrected_IMU['acc_pa'], label='medial-lateral', color='blue')
+pl2.set_xlabel('Index')
+pl2.set_ylabel('Acc')
+pl2.set_title('Post')
+pl2.legend()
+
+plt.tight_layout()
+plt.show()
 
 # %%
 # Applying the algorithm in wrongly oriented data
 # ----------------------
 # Below we apply the reorientation block to a lab trial with wrongly oriented data.
 # The signal comes from a sensor placed Clockwise with a 90-degree rotation
-# TODO: add "C90"
+
+# Loading the data for C90
+path = 'example_data/data_csv/data_reorientation/C90.csv'  # Adjust the file path
+IMU_C90 = pd.read_csv(path)
+
+# Assuming CorrectOrientationSensorAxes is defined elsewhere in your code
+reorientation = CorrectOrientationSensorAxes(sampling_rate_hz=100)
+corrected = reorientation.update_orientation(IMU_C90)
+
+corrected_IMU = corrected.corIMUdata
+corrected_IMU_sequence = corrected.corIMUdataSequence
 
 # Visualizing the signal before and after reorientation
-# Below we visualize the signal before and after reorientation in the above file
+fig, (pl1, pl2) = plt.subplots(2, 1, figsize=(10, 12))
+
+# Pre reorientation
+pl1.plot(IMU_C90.index, IMU_C90['acc_is'], label='inferior-superior', color='green')
+pl1.plot(IMU_C90.index, IMU_C90['acc_ml'], label='posterior-anterior', color='red')
+pl1.plot(IMU_C90.index, IMU_C90['acc_pa'], label='medial-lateral', color='blue')
+pl1.set_xlabel('Index')
+pl1.set_ylabel('Acc')
+pl1.set_title('Pre')
+pl1.legend()
+
+# Post reorientation
+pl2.plot(corrected_IMU['index'], corrected_IMU['acc_is'], label='inferior-superior', color='green')
+pl2.plot(corrected_IMU['index'], corrected_IMU['acc_ml'], label='posterior-anterior', color='red')
+pl2.plot(corrected_IMU['index'], corrected_IMU['acc_pa'], label='medial-lateral', color='blue')
+pl2.set_xlabel('Index')
+pl2.set_ylabel('Acc')
+pl2.set_title('Post')
+pl2.legend()
+
+plt.tight_layout()
+plt.show()
 
 # %%
 # Applying the algorithm in wrongly oriented data
 # ----------------------
 # Below we apply the reorientation block to a lab trial with wrongly oriented data.
 # The signal comes from a sensor placed Clockwise with a 90-degree and inside out
-# TODO: add "C90IO"
+
+# Loading the data for C90IO
+path = 'example_data/data_csv/data_reorientation/C90IO.csv'  # Adjust the file path
+IMU_C90IO = pd.read_csv(path)
+
+# Assuming CorrectOrientationSensorAxes is defined elsewhere in your code
+reorientation = CorrectOrientationSensorAxes(sampling_rate_hz=100)
+corrected = reorientation.update_orientation(IMU_C90IO)
+
+corrected_IMU = corrected.corIMUdata
+corrected_IMU_sequence = corrected.corIMUdataSequence
 
 # Visualizing the signal before and after reorientation
-# Below we visualize the signal before and after reorientation in the above file
+fig, (pl1, pl2) = plt.subplots(2, 1, figsize=(10, 12))
+
+# Pre reorientation
+pl1.plot(IMU_C90IO.index, IMU_C90IO['acc_is'], label='inferior-superior', color='green')
+pl1.plot(IMU_C90IO.index, IMU_C90IO['acc_ml'], label='posterior-anterior', color='red')
+pl1.plot(IMU_C90IO.index, IMU_C90IO['acc_pa'], label='medial-lateral', color='blue')
+pl1.set_xlabel('Index')
+pl1.set_ylabel('Acc')
+pl1.set_title('Pre')
+pl1.legend()
+
+# Post reorientation
+pl2.plot(corrected_IMU['index'], corrected_IMU['acc_is'], label='inferior-superior', color='green')
+pl2.plot(corrected_IMU['index'], corrected_IMU['acc_ml'], label='posterior-anterior', color='red')
+pl2.plot(corrected_IMU['index'], corrected_IMU['acc_pa'], label='medial-lateral', color='blue')
+pl2.set_xlabel('Index')
+pl2.set_ylabel('Acc')
+pl2.set_title('Post')
+pl2.legend()
+
+plt.tight_layout()
+plt.show()
 
 # %%
 # Applying the algorithm in wrongly oriented data
 # ----------------------
 # Below we apply the reorientation block to a lab trial with wrongly oriented data.
 # The signal comes from a sensor placed counter-clockwise with a 90-degree rotation
-# TODO: add "CC90"
+
+# Loading the data for CC90
+path = 'example_data/data_csv/data_reorientation/CC90.csv'  # Adjust the file path
+IMU_CC90 = pd.read_csv(path)
+
+# Assuming CorrectOrientationSensorAxes is defined elsewhere in your code
+reorientation = CorrectOrientationSensorAxes(sampling_rate_hz=100)
+corrected = reorientation.update_orientation(IMU_CC90)
+
+corrected_IMU = corrected.corIMUdata
+corrected_IMU_sequence = corrected.corIMUdataSequence
 
 # Visualizing the signal before and after reorientation
-# Below we visualize the signal before and after reorientation in the above file
+fig, (pl1, pl2) = plt.subplots(2, 1, figsize=(10, 12))
+
+# Pre reorientation
+pl1.plot(IMU_CC90.index, IMU_CC90['acc_is'], label='inferior-superior', color='green')
+pl1.plot(IMU_CC90.index, IMU_CC90['acc_ml'], label='posterior-anterior', color='red')
+pl1.plot(IMU_CC90.index, IMU_CC90['acc_pa'], label='medial-lateral', color='blue')
+pl1.set_xlabel('Index')
+pl1.set_ylabel('Acc')
+pl1.set_title('Pre')
+pl1.legend()
+
+# Post reorientation
+pl2.plot(corrected_IMU['index'], corrected_IMU['acc_is'], label='inferior-superior', color='green')
+pl2.plot(corrected_IMU['index'], corrected_IMU['acc_ml'], label='posterior-anterior', color='red')
+pl2.plot(corrected_IMU['index'], corrected_IMU['acc_pa'], label='medial-lateral', color='blue')
+pl2.set_xlabel('Index')
+pl2.set_ylabel('Acc')
+pl2.set_title('Post')
+pl2.legend()
+
+plt.tight_layout()
+plt.show()
 
 # %%
 # Applying the algorithm in wrongly oriented data
 # ----------------------
 # Below we apply the reorientation block to a lab trial with wrongly oriented data.
 # The signal comes from a sensor placed counter-clockwise with a 90-degree rotation and inside out
-# TODO: add "CC90IO"
+
+# Loading the data for CC90IO
+path = 'example_data/data_csv/data_reorientation/CC90IO.csv'  # Adjust the file path
+IMU_CC90IO = pd.read_csv(path)
+
+# Assuming CorrectOrientationSensorAxes is defined elsewhere in your code
+reorientation = CorrectOrientationSensorAxes(sampling_rate_hz=100)
+corrected = reorientation.update_orientation(IMU_CC90IO)
+
+corrected_IMU = corrected.corIMUdata
+corrected_IMU_sequence = corrected.corIMUdataSequence
 
 # Visualizing the signal before and after reorientation
-# Below we visualize the signal before and after reorientation in the above file
+fig, (pl1, pl2) = plt.subplots(2, 1, figsize=(10, 12))
+
+# Pre reorientation
+pl1.plot(IMU_CC90IO.index, IMU_CC90IO['acc_is'], label='inferior-superior', color='green')
+pl1.plot(IMU_CC90IO.index, IMU_CC90IO['acc_ml'], label='posterior-anterior', color='red')
+pl1.plot(IMU_CC90IO.index, IMU_CC90IO['acc_pa'], label='medial-lateral', color='blue')
+pl1.set_xlabel('Index')
+pl1.set_ylabel('Acc')
+pl1.set_title('Pre')
+pl1.legend()
+
+# Post reorientation
+pl2.plot(corrected_IMU['index'], corrected_IMU['acc_is'], label='inferior-superior', color='green')
+pl2.plot(corrected_IMU['index'], corrected_IMU['acc_ml'], label='posterior-anterior', color='red')
+pl2.plot(corrected_IMU['index'], corrected_IMU['acc_pa'], label='medial-lateral', color='blue')
+pl2.set_xlabel('Index')
+pl2.set_ylabel('Acc')
+pl2.set_title('Post')
+pl2.legend()
+
+plt.tight_layout()
+plt.show()
