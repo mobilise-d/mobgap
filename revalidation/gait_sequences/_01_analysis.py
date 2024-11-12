7ee36ca83f1cb870728b32184af993a13f2f755a
Arne Küderle <a.kuederle@gmail.com>
2024-11-12 16:40:37 +0100

    Added huge disclaimer for validation results

--- revalidation/gait_sequences/_01_analysis.py
+++ revalidation/gait_sequences/_01_analysis.py
@@ -1,4 +1,11 @@
 """
+.. warning:: On this page you will find preliminary results for a standardized revalidation of the pipeline and all
+  of its algorithm.
+  The current state, **TECHNICAL EXPERIMENTATION**.
+  Don't use these results or make any assumptions based on them.
+  We will update this page incrementatlly and provide further information, as soon as the state of any of the validation
+  steps changes.
+
 .. _gsd_val_results
 
 Performance of the gait sequences algorithm on the TVS dataset
