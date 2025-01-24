e7ed00e41f7d776d93ddc26f680a05a66e9ce1d7
Arne Küderle <a.kuederle@gmail.com>
2025-01-24 13:28:12 +0100

    FIxed daylight saving bug with weartime reports

--- tests/test_data/test_weartime.py
+++ tests/test_data/test_weartime.py
@@ -1,12 +1,22 @@
 from pathlib import Path
 
+import pytest
+from statsmodels.compat.pandas import assert_series_equal
+
 from mobgap.data._mobilsed_weartime_loader import load_weartime_from_daily_mcroberts_report
 
+HERE = Path(__file__).parent
 
-def test_basic_weartime():
-    test = load_weartime_from_daily_mcroberts_report(
-        Path(
-            "/home/arne/Documents/repos/work/mobilised/agg_dmo_data/T3/T3_MinutetByMinute-21May24/20240521_Mobilise-D_CVS_T3_MinutetoMinuteWearTime_classification_23116_minute.csv"
-        )
+
+@pytest.mark.parametrize("waking_hours", [(0, 24), (1, 23), (7, 22)])
+def test_basic_weartime(snapshot, waking_hours):
+    output = load_weartime_from_daily_mcroberts_report(
+        HERE / "data/weartime/example_weartime_min_by_min_report.csv", waking_hours
     )
-    test
+    snapshot.assert_match(output.reset_index().astype({"visit_date": "string"}), name=waking_hours)
+
+    max_hours = waking_hours[1] - waking_hours[0] + 1
+    assert (output.total_worn_during_waking_h <= max_hours).all()
+
+    if waking_hours == (0, 24):
+        assert_series_equal(output.total_worn_h, output.total_worn_during_waking_h, check_names=False)
