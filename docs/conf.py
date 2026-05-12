df20acf1b00d4d687e6de157131251de21614ffd
DMegaritis <dmgrei3@gmail.com>
2026-05-12 08:44:04 +0100

    Add examples for reorientation and wear-time detection to docs configuration

--- docs/conf.py
+++ docs/conf.py
@@ -210,6 +210,7 @@ sphinx_gallery_conf = {
             "../examples/turning",
             "../examples/wba",
             "../examples/aggregation",
+            "../examples/re_orientation",
             "../examples/data_transform",
             "../examples/dev_guides",
             "../revalidation/full_pipeline",
