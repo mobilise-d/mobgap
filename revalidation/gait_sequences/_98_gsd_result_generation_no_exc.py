3ba7a6c448b77af9a494c1fd7d4b4576e3af9a9d
Arne Küderle <a.kuederle@gmail.com>
2026-06-12 14:35:57 +0200

    test: cover adaptive iluz equivalence

--- revalidation/gait_sequences/_98_gsd_result_generation_no_exc.py
+++ revalidation/gait_sequences/_98_gsd_result_generation_no_exc.py
@@ -67,39 +67,35 @@ matlab_algo_result_path = (
 )
 
 pipelines = {}
-# Temporarily disabled while regenerating only GsdIluzAdaptiveGravity results.
-# for matlab_algo_name in [
-#     "EPFL_V1-improved_th",
-#     "EPFL_V1-original",
-#     "EPFL_V2-original",
-#     # "Gaitpy",
-#     # "Hickey-original",
-#     # "Rai",
-#     "TA_Iluz-original",
-#     # "TA_Wavelets_v2",
-# ]:
-#     pipelines[f"matlab_{matlab_algo_name}"] = GsdEmulationPipeline(
-#         DummyGsdAlgo(
-#             matlab_algo_name, base_result_folder=matlab_algo_result_path
-#         )
-#     )
+for matlab_algo_name in [
+    "EPFL_V1-improved_th",
+    "EPFL_V1-original",
+    "EPFL_V2-original",
+    # "Gaitpy",
+    # "Hickey-original",
+    # "Rai",
+    "TA_Iluz-original",
+    # "TA_Wavelets_v2",
+]:
+    pipelines[f"matlab_{matlab_algo_name}"] = GsdEmulationPipeline(
+        DummyGsdAlgo(
+            matlab_algo_name, base_result_folder=matlab_algo_result_path
+        )
+    )
 
 # %%
 # For the reimplemented algorithm, we set up version with different default presets.
-from mobgap.gait_sequences import GsdIluzAdaptiveGravity
+from mobgap.gait_sequences import GsdAdaptiveIonescu, GsdIluz, GsdIluzAdaptiveGravity, GsdIonescu
 
+pipelines["GsdIluz"] = GsdEmulationPipeline(GsdIluz())
 pipelines["GsdIluzAdaptiveGravity"] = GsdEmulationPipeline(
     GsdIluzAdaptiveGravity(), convert_to_body_frame=False
 )
-# Temporarily disabled while regenerating only GsdIluzAdaptiveGravity results.
-# from mobgap.gait_sequences import GsdAdaptiveIonescu, GsdIluz, GsdIonescu
-#
-# pipelines["GsdIluz"] = GsdEmulationPipeline(GsdIluz())
-# pipelines["GsdIluz_orig_peak"] = GsdEmulationPipeline(
-#     GsdIluz(**GsdIluz.PredefinedParameters.original)
-# )
-# pipelines["GsdIonescu"] = GsdEmulationPipeline(GsdIonescu())
-# pipelines["GsdAdaptiveIonescu"] = GsdEmulationPipeline(GsdAdaptiveIonescu())
+pipelines["GsdIluz_orig_peak"] = GsdEmulationPipeline(
+    GsdIluz(**GsdIluz.PredefinedParameters.original)
+)
+pipelines["GsdIonescu"] = GsdEmulationPipeline(GsdIonescu())
+pipelines["GsdAdaptiveIonescu"] = GsdEmulationPipeline(GsdAdaptiveIonescu())
 
 # %%
 # Setting up the dataset
